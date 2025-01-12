bda9c9fbbad09261c8d431c73969fd8e3324958e
GerardoFJ <A00838645@tec.mx>
2025-01-12 05:57:43 -0700

    Added gpd and wrapper v1

---
+++ manipulacion/packages/arm_pkg/src/gpd_test.cpp
@@ -0,0 +1,48 @@
+#include <gpd/grasp_detector.h>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include <gpd/util/cloud.h>
+#include "std_srvs/srv/trigger.hpp" 
+
+class grasp_detect : public rclcpp::Node
+{  
+    public:
+    grasp_detect(): Node("grasp_detect")
+    {
+        grasp_detect_srv_ = this->create_service<std_srvs::srv::Trigger>("grasp_detect", std::bind(&grasp_detect::service_callback, this,std::placeholders::_1, std::placeholders::_2));
+    
+    }
+ 
+  private:
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr grasp_detect_srv_;
+
+    void service_callback(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+                          std::shared_ptr<std_srvs::srv::Trigger::Response> response)
+    {
+        RCLCPP_INFO(this->get_logger(), "Getting pose");
+        std::string config_filename = "/home/gerardo/Roborregos/Repos/home_repo/src/home2/manipulacion/packages/gpd/cfg/eigen_params.cfg";
+        std::string pathcloud = "/home/gerardo/Roborregos/Repos/home_repo/src/home2/manipulacion/packages/gpd/tutorials/krylon.pcd";
+        gpd::util::Cloud cloud(pathcloud, Eigen::Matrix3Xd::Zero(3, 1));
+        gpd::GraspDetector detector(config_filename);
+        detector.preprocessPointCloud(cloud);
+        auto result = detector.detectGrasps(cloud);
+    
+        if(!result.empty()){
+          result[0]->print();
+        }
+        
+        response->success = true;
+
+
+    }
+   
+
+}; 
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<grasp_detect>());
+  rclcpp::shutdown();
+  return 0;
+}
