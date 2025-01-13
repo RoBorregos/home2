4ce90962d65b184629f46ada93de2f6cb8b78788
EmilianoHFlores <A00833160@tex.mx>
2025-01-13 10:59:43 -0600

    Add manipulation run script to run.sh

--- run.sh
+++ run.sh
@@ -8,11 +8,45 @@ fi
 
 echo "Service name is: $INPUT"
 
+# check arguments passed as --help or -h
+if [ "$INPUT" == "--help" ] || [ "$INPUT" == "-h" ]; then
+  echo "Usage: ./run.sh [service_name]"
+  echo "Example: ./run.sh vision"
+  exit 0
+fi
+
+area=""
+rebuild=0
+
 case $INPUT in
   vision)
     echo "Running vision..."
-    cd docker/vision
-    ./run.sh
+    area="vision"
     ;;
-
+  manipulation)
+    echo "Running manipulation"
+    area="manipulation"
+    ;;
+  rebuild)
+    echo "Rebuilding all services..."
+    rebuild=1
+    ;;
+  *)
+    echo "Invalid service name provided. Valid args are: vision, hri, etc"
+    exit 1
+    ;;
 esac
+
+if [ -z "$area" ]; then
+  echo "Invalid service name provided. Valid args are: vision, hri, etc"
+  exit 1
+fi
+
+cd docker/$area
+if [ $rebuild -eq 1 ]; then
+  echo "Rebuilding image from area: $area"
+  ./run.sh --rebuild
+else
+  echo "Running image from area: $area"
+  ./run.sh
+fi
