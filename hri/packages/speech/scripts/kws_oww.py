40ba9b132fbc1e7c5fe8958788a4959d09f7300e
JocelynVelarde <a01285780@itesm.mx>
2025-01-15 17:32:33 -0600

    update lint changes

--- hri/packages/speech/scripts/kws_oww.py
+++ hri/packages/speech/scripts/kws_oww.py
@@ -3,9 +3,8 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool
 from frida_interfaces.msg import AudioData
-import numpy as np
-import openwakeword
 from openwakeword.model import Model
+
 
 class OpenWakeWordNode(Node):
     def __init__(self):
@@ -19,10 +18,18 @@ class OpenWakeWordNode(Node):
         self.declare_parameter("chunk_size", 1280)
 
         model_path = self.get_parameter("model_path").get_parameter_value().string_value
-        inference_framework = self.get_parameter("inference_framework").get_parameter_value().string_value
-        audio_topic = self.get_parameter("audio_topic").get_parameter_value().string_value
-        detection_topic = self.get_parameter("detection_topic").get_parameter_value().string_value
-        self.chunk_size = self.get_parameter("chunk_size").get_parameter_value().integer_value
+        inference_framework = (
+            self.get_parameter("inference_framework").get_parameter_value().string_value
+        )
+        audio_topic = (
+            self.get_parameter("audio_topic").get_parameter_value().string_value
+        )
+        detection_topic = (
+            self.get_parameter("detection_topic").get_parameter_value().string_value
+        )
+        self.chunk_size = (
+            self.get_parameter("chunk_size").get_parameter_value().integer_value
+        )
 
         # Initialize OpenWakeWord
         if model_path:
@@ -45,19 +52,23 @@ class OpenWakeWordNode(Node):
 
     def audio_callback(self, msg):
         """Process incoming audio data and detect wakewords."""
-        # Convert audio data from ROS to NumPy array
-        audio_data = np.frombuffer(msg.data, dtype=np.int16)
-
+        # Convert audio data from ROS to NumPy array (to obtain directly from microphone)
+        # audio_data = np.frombuffer(msg.data, dtype=np.int16)
         # Perform prediction using OpenWakeWord (returns numerical value)
-        prediction = self.oww_model.predict(audio_data)
+        # prediction = self.oww_model.predict(audio_data)
 
         # Check for wakeword detection ("YES", "NO", "Frida")
         for keyword, buffer in self.oww_model.prediction_buffer.items():
             scores = list(buffer)
-            if scores[-1] > 0.5:  # NOTE: This value can be decreased for more sensitive yet less effective detection
-                self.get_logger().info(f"Wakeword '{keyword}' detected with score {scores[-1]:.2f}")
+            if (
+                scores[-1] > 0.5
+            ):  # NOTE: This value can be decreased for more sensitive yet less effective detection
+                self.get_logger().info(
+                    f"Wakeword '{keyword}' detected with score {scores[-1]:.2f}"
+                )
                 self.publisher.publish(Bool(data=True))
                 break
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -68,6 +79,7 @@ def main(args=None):
         pass
     finally:
         rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
