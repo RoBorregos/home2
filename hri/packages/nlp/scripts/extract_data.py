37fabd5cb4eaa78b4d7f6966176a65105494bc83
IvanRomero03 <iwells.ar@gmail.com>
2025-01-15 14:25:36 -0800

    ruff

--- hri/packages/nlp/scripts/extract_data.py
+++ hri/packages/nlp/scripts/extract_data.py
@@ -30,26 +30,37 @@ class DataExtractor(Node):
         self.get_logger().info("Starting data extractor node")
 
         self.srv = self.create_service(
-            ExtractInfo, EXTRACT_DATA_SERVICE, self.extract_info_requested)
+            ExtractInfo, EXTRACT_DATA_SERVICE, self.extract_info_requested
+        )
 
         openai.api_key = os.getenv("OPENAI_API_KEY")
         self.get_logger().info("Data extractor node started")
 
-    def extract_info_requested(self, request: ExtractInfo.Request, response: ExtractInfo.Response) -> ExtractInfo.Response:
+    def extract_info_requested(
+        self, request: ExtractInfo.Request, response: ExtractInfo.Response
+    ) -> ExtractInfo.Response:
         """Service to extract information from text."""
 
         self.get_logger().info("Extracting information from text")
 
         instruction = "You will be presented with some text and data to extract. Please provide the requested information or leave empty if it isn't available."
-        response_content = openai.beta.chat.completions.parse(
-            model="gpt-4o-2024-08-06",
-            messages=[
-                {"role": "system", "content": instruction},
-                {"role": "user", "content": str(request.full_text) +
-                    "Data to extract: " + str(request.data)}
-            ],
-            response_format=ExtractedData
-        ).choices[0].message.content
+        response_content = (
+            openai.beta.chat.completions.parse(
+                model="gpt-4o-2024-08-06",
+                messages=[
+                    {"role": "system", "content": instruction},
+                    {
+                        "role": "user",
+                        "content": str(request.full_text)
+                        + "Data to extract: "
+                        + str(request.data),
+                    },
+                ],
+                response_format=ExtractedData,
+            )
+            .choices[0]
+            .message.content
+        )
 
         self.get_logger().info(f"Extracted data: {response_content}")
 
@@ -58,8 +69,7 @@ class DataExtractor(Node):
             result = ExtractedData(**response_data)
         except Exception as e:
             self.get_logger().error(f"Service error: {e}")
-            raise rclpy.exceptions.ServiceException(
-                str(e))
+            raise rclpy.exceptions.ServiceException(str(e))
 
         response.result = result.data
         return response
