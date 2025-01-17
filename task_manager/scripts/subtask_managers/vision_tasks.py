3363667534c4b615ed6e9b372bb5795c4f05abcc
Ale-Coeto <alecoetos@gmail.com>
2025-01-17 03:09:06 -0600

    Proposal for subtask managers

--- task_manager/scripts/subtask_managers/vision_tasks.py
+++ task_manager/scripts/subtask_managers/vision_tasks.py
@@ -14,6 +14,8 @@ from frida_interfaces.srv import SaveName
 from frida_interfaces.srv import FindSeat
 from frida_interfaces.action import DetectPerson
 
+from utils.decorators import mockable, service_check
+
 SAVE_NAME_TOPIC = "/vision/new_name"
 FIND_SEAT_TOPIC = "/vision/find_seat"
 DETECT_PERSON_TOPIC = "/vision/detect_person"
@@ -26,8 +28,9 @@ class VisionTasks:
 
     STATE = {"TERMINAL_ERROR": -1, "EXECUTION_ERROR": 0, "EXECUTION_SUCCESS": 1}
 
-    def __init__(self, task_manager) -> None:
+    def __init__(self, task_manager, mock_data=False) -> None:
         self.node = task_manager
+        self.mock_data = mock_data
 
         self.save_name_client = self.node.create_client(SaveName, SAVE_NAME_TOPIC)
         self.find_seat_client = self.node.create_client(FindSeat, FIND_SEAT_TOPIC)
@@ -35,6 +38,10 @@ class VisionTasks:
             self.node, DetectPerson, DETECT_PERSON_TOPIC
         )
 
+        self.setup_services()
+
+    def setup_services(self):
+        """Initialize services and actions"""
         if not self.save_name_client.wait_for_service(timeout_sec=TIMEOUT):
             self.node.get_logger().warn(
                 "Save name service not initialized. (face_recognition)"
@@ -54,11 +61,10 @@ class VisionTasks:
         """Print success message"""
         self.node.get_logger().info(f"\033[92mSUCCESS:\033[0m {message}")
 
+    @service_check("save_name_client", -1, TIMEOUT)
+    @mockable(default_value=100)
     def save_face_name(self, name: str) -> int:
         """Save the name of the person detected"""
-        if not self.save_name_client.wait_for_service(timeout_sec=TIMEOUT):
-            self.node.get_logger().error("Save name service not available")
-            return self.STATE["TERMINAL_ERROR"]
 
         self.node.get_logger().info(f"Saving name: {name}")
         request = SaveName.Request()
@@ -79,11 +85,10 @@ class VisionTasks:
         self.success(f"Name saved: {name}")
         return self.STATE["EXECUTION_SUCCESS"]
 
+    @service_check("find_seat_client", 300, TIMEOUT)
+    @mockable(default_value=100)
     def find_seat(self) -> int:
         """Find an available seat and get the angle for the camera to point at"""
-        if not self.find_seat_client.wait_for_service(timeout_sec=TIMEOUT):
-            self.node.get_logger().error("Find seat service not available")
-            return 300
 
         self.node.get_logger().info("Finding available seat")
         request = FindSeat.Request()
@@ -104,11 +109,10 @@ class VisionTasks:
 
         return 300
 
+    @service_check("detect_person_action_client", False, TIMEOUT)
+    @mockable(default_value=True, delay=2)
     def detect_person(self) -> bool:
         """Returns true when a person is detected"""
-        if not self.detect_person_action_client.wait_for_server(timeout_sec=TIMEOUT):
-            self.node.get_logger().error("Detect person action server not available")
-            return False
 
         self.node.get_logger().info("Waiting for person detection")
         goal = DetectPerson.Goal()
