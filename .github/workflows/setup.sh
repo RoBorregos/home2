de7a7c2a9479719737c179ad4050b8c24c157082
GerardoFJ <A00838645@tec.mx>
2025-01-17 15:23:04 -0700

    testing workflow

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "APT UPDATE" 
 sudo apt update
-DEBIAN_FRONTEND=noninteractive 
-
+DEBIAN_FRONTEND=noninteractive
+ 
+echo "ROSDEP INIT" 
 rosdep init
+
+echo "ROSDEP UPDATE"
 rosdep update
 
 #install vcs-tools
@@ -11,7 +15,14 @@ DEBIAN_FRONTEND=noninteractive
 
 #rosdep install
 DEBIAN_FRONTEND=noninteractive 
+
+#For testing 
+echo "LS MANIPULATION"
+ls ./manipulation/package/
+
+echo "ROSDEP INSTALL"
 rosdep install --from-paths . -y --ignore-src -r --skip-keys gpd
 
 #Running prebuild
+echo "PREBUILD SCRIPT"
 bash ./prebuild.sh -y
