80546b4f0702f032857ce577483bae040453ab12
GerardoFJ <A00838645@tec.mx>
2025-01-17 14:51:06 -0700

    added prebuild workflow compability

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -11,7 +11,10 @@ DEBIAN_FRONTEND=noninteractive
 
 #rosdep install
 DEBIAN_FRONTEND=noninteractive 
-rosdep install --from-paths . -y --ignore-src -r
+rosdep install --from-paths . -y --ignore-src -r --skip-keys gpd
+
+#Running prebuild
+bash ./prebuild.sh -y
 
 # - Importing all dependencies
 colcon build --symlink-install
