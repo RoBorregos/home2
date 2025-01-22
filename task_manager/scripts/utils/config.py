fad2132ab1c1b52b3eec8850758e7c2c5f0df4d2
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-01-21 20:04:07 -0600

    feat: install config using cmakelists

--- task_manager/scripts/utils/config.py
+++ task_manager/scripts/utils/config.py
@@ -15,14 +15,24 @@ class TopicConfig(BaseModel):
     enabled: Optional[bool] = False
     type: TypeEnum
 
+    class Config:
+        extra = "forbid"
+
 
 class MockConfig(BaseModel):
     function_name: str
     enabled: Optional[bool] = False
     mock_data: Any
 
+    class Config:
+        extra = "forbid"
+
 
 class SubtaskConfig(BaseModel):
     topic_config: List[TopicConfig]
     mock_config: Optional[List[MockConfig]]
     strict: Optional[bool] = True
+
+    # Don't allow extra config values
+    class Config:
+        extra = "forbid"
