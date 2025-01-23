d9063df63a03ea296824997d06cbd093b7123c2b
GerardoFJ <A00838645@tec.mx>
2025-01-22 22:27:29 -0700

    test_env

---
+++ manipulation/packages/arm_pkg/scripts/test_env.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import ExternalShutdownException
+import os
+
+
+class TestNode(Node):
+    def __init__(self):
+        super().__init__("test_node")
+        self.get_logger().info("*Starting Hear Node*")
+        self.timer = self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger.info(os.environ["SRC_HOME_PATH"])
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    if os.environ["SRC_HOME_PATH"]:
+        try:
+            rclpy.spin(TestNode())
+        except (ExternalShutdownException, KeyboardInterrupt):
+            pass
+        finally:
+            rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
