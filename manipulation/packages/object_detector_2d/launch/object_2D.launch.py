7300eb668c02145a612cf7bfa2e83d66fd5087df
AleGonzcamilla <A00839233@tec.mx>
2025-01-30 16:54:34 -0600

    started msg in frida interfaces}

---
+++ manipulation/packages/object_detector_2d/launch/object_2D.launch.py
@@ -0,0 +1,22 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config = os.path.join(
+      get_package_share_directory('object_detector_2d'),
+      'config',
+      'parameters.yaml'
+      )
+    return LaunchDescription([
+        Node(
+            package='object_detector_2d',
+            executable='Detection2D.py',
+            name='Detection2D',
+            respawn=True,
+            output='screen',
+            emulate_tty=True,
+            parameters=[config],
+        ),
+    ])
