07c487a7519f1bb37d35d243aa72cbd4e5eafbe9
GerardoFJ <A00838645@tec.mx>
2025-02-13 02:11:10 -0600

    Changes and docker

--- task_manager/scripts/demo_task_manager.py
+++ task_manager/scripts/demo_task_manager.py
@@ -59,48 +59,55 @@ class DemoTaskManager(Node):
         self.current_state = DemoTaskManager.TASK_STATES["FOLLOW_FACE"]
 
         self.get_logger().info("DemoTaskManager has started.")
-        self.create_timer(1.0, self.run)
+        self.create_timer(0.1, self.run)
 
     def run(self):
         """Running main loop"""
 
-        while rclpy.ok():
-            if self.current_state == DemoTaskManager.TASK_STATES["START"]:
-                Logger.state(self, "Starting task")
-                self.subtask_manager.hri.say(
-                    "Hi, I'm FRIDA, a service robot designed by RoBorregos. I can do several requests, just say my name to chat."
-                )
-                self.current_state = DemoTaskManager.TASK_STATES["FOLLOW_FACE"]
+        if self.current_state == DemoTaskManager.TASK_STATES["START"]:
+            Logger.state(self, "Starting task")
+            self.subtask_manager.hri.say(
+                "Hi, I'm FRIDA, a service robot designed by RoBorregos. I can do several requests, just say my name to chat."
+            )
+            self.current_state = DemoTaskManager.TASK_STATES["FOLLOW_FACE"]
 
-            if self.current_state == DemoTaskManager.TASK_STATES["INTRODUCTION"]:
-                Logger.state(self, "Introduction task")
-                # Wait until keyword is said
-                self.subtask_manager.hri.say("Hi, I'm FRIDA, what is your name?")
-                name = self.subtask_manager.hri.hear()
-                self.subtask_manager.vision.save_face_name(name)
-                self.subtask_manager.hri.say(f"Hello {name}, how can i help you?")
-                self.current_state = DemoTaskManager.TASK_STATES["RECEIVE_COMMAND"]
+        if self.current_state == DemoTaskManager.TASK_STATES["INTRODUCTION"]:
+            Logger.state(self, "Introduction task")
+            # Wait until keyword is said
+            self.subtask_manager.hri.say("Hi, I'm FRIDA, what is your name?",wait=True)
+            text = self.subtask_manager.hri.hear()
+            name = self.subtask_manager.hri.extract_data("name", text)
+            self.subtask_manager.vision.save_face_name(name)
+            self.subtask_manager.hri.say(f"Hello {name}, how can i help you?",wait=True)
+            self.current_state = DemoTaskManager.TASK_STATES["RECEIVE_COMMAND"]
 
         if self.current_state == DemoTaskManager.TASK_STATES["RECEIVE_COMMAND"]:
             # Do sth to receive and parse basic commands (go to, pick, place)
             Logger.state(self, "Receive command task")
+            text = self.subtask_manager.hri.hear()
+            response = self.subtask_manager.hri.ask(text)
+            self.subtask_manager.hri.say(response)
 
-            if self.current_state == DemoTaskManager.TASK_STATES["FOLLOW_FACE"]:
-                # Follow face task
-                Logger.state(self, "Follow face task")
-                if self.subtask_manager.vision.follow_face is None:
-                    continue
-                x, y = self.subtask_manager.vision.follow_face
-                new_x = min(
-                    max(x * self.x_delta_multiplier + self.current_x, self.min_x), self.max_x
-                )
+        if self.current_state == DemoTaskManager.TASK_STATES["FOLLOW_FACE"]:
+            # Follow face task
+            Logger.state(self, "Follow face task")
+            if self.subtask_manager.vision.follow_face is None:
+                continue
+            x, y = self.subtask_manager.vision.follow_face
+            new_x = min(
+                max(x * self.x_delta_multiplier + self.current_x, self.min_x), self.max_x
+            )
 
-                new_y = min(
-                    max(y * self.y_delta_multiplier + self.current_y, self.min_y), self.max_y
-                )
+            new_y = min(
+                max(y * self.y_delta_multiplier + self.current_y, self.min_y), self.max_y
+            )
 
-                if new_x != self.current_x or new_y != self.current_y:
-                    self.subtask_manager.manipulation.move_to(new_x, new_y)
+            if new_x != self.current_x or new_y != self.current_y:
+                self.subtask_manager.manipulation.move_to(new_x, new_y)
+
+            if self.subtask_manager.hri.keyword() = "frida":
+                self.current_state = DemoTaskManager.TASK_STATES["INTRODUCTION"]
+
 
 
 def main(args=None):
