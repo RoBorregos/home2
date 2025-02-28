05717a73271fd4f5143e5a5303a2858394ac8f87
GerardoFJ <A00838645@tec.mx>
2025-02-28 03:06:50 +0100

    odom chida

--- navigation/packages/dashgo_driver/scripts/dashgo_driver2.py
+++ navigation/packages/dashgo_driver/scripts/dashgo_driver2.py
@@ -10,33 +10,35 @@ from math import pi as PI, degrees, radians, sin, cos
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
 from tf2_ros import TransformBroadcaster
+from rclpy.duration import Duration
 
-ODOM_POSE_COVARIANCE = [1e-3, 0, 0, 0, 0, 0, 
-                        0, 1e-3, 0, 0, 0, 0,
-                        0, 0, 1e6, 0, 0, 0,
-                        0, 0, 0, 1e6, 0, 0,
-                        0, 0, 0, 0, 1e6, 0,
-                0, 0, 0, 0, 0, 1e3]
-ODOM_POSE_COVARIANCE2 = [1e-9, 0, 0, 0, 0, 0, 
-                         0, 1e-3, 1e-9, 0, 0, 0,
-                         0, 0, 1e6, 0, 0, 0,
-                         0, 0, 0, 1e6, 0, 0,
-                         0, 0, 0, 0, 1e6, 0,
-                         0, 0, 0, 0, 0, 1e-9]
+ODOM_POSE_COVARIANCE = [float(1e-3), 0.0, 0.0, 0.0, 0.0, 0.0, 
+                        0.0, float(1e-3), 0.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, float(1e6), 0.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0, float(1e6), 0.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, float(1e6), 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, float(1e3)]
 
-ODOM_TWIST_COVARIANCE = [1e-3, 0, 0, 0, 0, 0, 
-                         0, 1e-3, 0, 0, 0, 0,
-                         0, 0, 1e6, 0, 0, 0,
-                         0, 0, 0, 1e6, 0, 0,
-                         0, 0, 0, 0, 1e6, 0,
-                         0, 0, 0, 0, 0, 1e3]
-ODOM_TWIST_COVARIANCE2 = [1e-9, 0, 0, 0, 0, 0, 
-                          0, 1e-3, 1e-9, 0, 0, 0,
-                          0, 0, 1e6, 0, 0, 0,
-                          0, 0, 0, 1e6, 0, 0,
-                          0, 0, 0, 0, 1e6, 0,   
-                          0, 0, 0, 0, 0, 1e-9]
+ODOM_POSE_COVARIANCE2 = [float(1e-9), 0.0, 0.0, 0.0, 0.0, 0.0, 
+                        0.0, float(1e-3), 0.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, float(1e6), 0.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0, float(1e6), 0.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, float(1e6), 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, float(1e-9)]
 
+ODOM_TWIST_COVARIANCE = [float(1e-3), 0.0, 0.0, 0.0, 0.0, 0.0, 
+                         0.0, float(1e-3), 0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, float(1e6), 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, float(1e6), 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0, float(1e6), 0.0,
+                         0.0, 0.0, 0.0, 0.0, 0.0, float(1e3)]
+
+ODOM_TWIST_COVARIANCE2 = [float(1e-9), 0.0, 0.0, 0.0, 0.0, 0.0, 
+                         0.0, float(1e-3), 0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, float(1e6), 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, float(1e6), 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0, float(1e6), 0.0,
+                         0.0, 0.0, 0.0, 0.0, 0.0, float(1e-9)]
 
 SERVO_MAX = 180
 SERVO_MIN = 0
@@ -74,6 +76,7 @@ class DashgoDriver(Node):
         self.l_wheel_mult = 0
         self.r_wheel_mult = 0
         self.now = self.get_clock().now()
+        self.then = self.now
         self.cmd_vel = Twist()
         self.odomBroadcaster = TransformBroadcaster(self)
         self.enc_left = None            # encoder readings
@@ -97,7 +100,8 @@ class DashgoDriver(Node):
         self.create_subscription(Twist,'cmd_vel',self.cmdVelCallback,10)
         self.imuPub = self.create_publisher(Imu, 'imu', 5)
         self.imuAnglePub = self.create_publisher(Float32, 'imu_angle', 5)
-        self.odomPub = self.create_publisher(Odometry, 'odom', 5)
+        #self.odomPub = self.create_publisher(Odometry, 'odom', 5)
+        self.odomPub = self.create_publisher(Odometry, 'dashgo_odom', 5)
 
         self.controller = Stm32(self.port, self.baud, self.timeout)
         self.controller.connect()
@@ -114,168 +118,175 @@ class DashgoDriver(Node):
         timer_period = 1 / self.rate  # seconds
         self.timer = self.create_timer(timer_period, self.base_controller)
 
-        def base_controller(self):
-            self.now = self.get_clock().now()
+    def base_controller(self):
+        self.now = self.get_clock().now()
+        try:
+            stat_, left_enc,right_enc = self.controller.get_encoder_counts()
+        except:
+            self.bad_encoder_count += 1
+            self.get_logger().info("Encoder exception count: " + str(self.bad_encoder_count))
+            return
+        if (self.useImu == True) :
             try:
-                stat_, left_enc,right_enc = self.controller.get_encoder_counts()
+                stat_, yaw, yaw_vel, acc_x, acc_y, acc_z = self.controller.get_imu_val()
+                if yaw>=18000:
+                    yaw = yaw-65535
+                yaw = yaw/100.0
+                if yaw_vel>=32768:
+                    yaw_vel = yaw_vel-65535
+                yaw_vel = yaw_vel/100.0
+                #rospy.loginfo("yaw: " + str(yaw/100)+" yaw_vel: " + str(yaw_vel))     
+                imu_data = Imu()  
+                imu_data.header.stamp = self.get_clock().now().to_msg()
+                imu_data.header.frame_id = self.imu_frame_id 
+                imu_data.orientation_covariance[0] = 1000000
+                imu_data.orientation_covariance[1] = 0
+                imu_data.orientation_covariance[2] = 0
+                imu_data.orientation_covariance[3] = 0
+                imu_data.orientation_covariance[4] = 1000000
+                imu_data.orientation_covariance[5] = 0
+                imu_data.orientation_covariance[6] = 0
+                imu_data.orientation_covariance[7] = 0
+                imu_data.orientation_covariance[8] = 0.000001
+                imu_quaternion = Quaternion()
+                imu_quaternion.x = 0.0 
+                imu_quaternion.y = 0.0
+                imu_quaternion.z = sin(-1*self.imu_offset*yaw*3.1416/(180 *2.0))
+                imu_quaternion.w = cos(-1*self.imu_offset*yaw*3.1416/(180 *2.0))
+                imu_data.orientation = imu_quaternion
+                imu_data.linear_acceleration_covariance[0] = -1
+                imu_data.angular_velocity_covariance[0] = -1
+
+                imu_data.angular_velocity.x = 0.0
+                imu_data.angular_velocity.y = 0.0
+                imu_data.angular_velocity.z = (yaw_vel*3.1416/(180*100))
+                self.imuPub.publish(imu_data)
+                angle = Float32()
+                angle.data = -1.0*self.imu_offset*yaw*3.1416/(180 *2.0)
+                self.imuAnglePub.publish(angle)
+
             except:
                 self.bad_encoder_count += 1
-                self.get_logger().info("Encoder exception count: " + str(self.bad_encoder_count))
+                self.get_logger().info("IMU exception count: " + str(self.bad_encoder_count))
                 return
-            if (self.useImu == True) :
-                try:
-                    stat_, yaw, yaw_vel, acc_x, acc_y, acc_z = self.controller.get_imu_val()
-                    if yaw>=18000:
-                        yaw = yaw-65535
-                    yaw = yaw/100.0
-                    if yaw_vel>=32768:
-                        yaw_vel = yaw_vel-65535
-                    yaw_vel = yaw_vel/100.0
-                    #rospy.loginfo("yaw: " + str(yaw/100)+" yaw_vel: " + str(yaw_vel))     
-                    imu_data = Imu()  
-                    imu_data.header.stamp = self.get_clock().now().msg()
-                    imu_data.header.frame_id = self.imu_frame_id 
-                    imu_data.orientation_covariance[0] = 1000000
-                    imu_data.orientation_covariance[1] = 0
-                    imu_data.orientation_covariance[2] = 0
-                    imu_data.orientation_covariance[3] = 0
-                    imu_data.orientation_covariance[4] = 1000000
-                    imu_data.orientation_covariance[5] = 0
-                    imu_data.orientation_covariance[6] = 0
-                    imu_data.orientation_covariance[7] = 0
-                    imu_data.orientation_covariance[8] = 0.000001
-                    imu_quaternion = Quaternion()
-                    imu_quaternion.x = 0.0 
-                    imu_quaternion.y = 0.0
-                    imu_quaternion.z = sin(-1*self.imu_offset*yaw*3.1416/(180 *2.0))
-                    imu_quaternion.w = cos(-1*self.imu_offset*yaw*3.1416/(180 *2.0))
-                    imu_data.orientation = imu_quaternion
-                    imu_data.linear_acceleration_covariance[0] = -1
-                    imu_data.angular_velocity_covariance[0] = -1
+            
+        dt = self.now - self.then
+        self.then = self.now
+        dt = dt.nanoseconds / 1e9
 
-                    imu_data.angular_velocity.x = 0.0
-                    imu_data.angular_velocity.y = 0.0
-                    imu_data.angular_velocity.z = (yaw_vel*3.1416/(180*100))
-                    self.imuPub.publish(imu_data)
-                    self.imuAnglePub.publish(-1*self.imu_offset*yaw*3.1416/(180 *2.0))
-       
-                except:
-                    self.bad_encoder_count += 1
-                    self.get_logger().info("IMU exception count: " + str(self.bad_encoder_count))
-                    return
-            dt = self.now - self.then
-            self.then = self.now
-            dt = dt.nanoseconds / 1e9
-
-            if self.enc_left == None:
-                dright = 0
-                dleft = 0
+        if self.enc_left == None:
+            dright = 0
+            dleft = 0
+        else:
+            if (left_enc < self.encoder_low_wrap and self.enc_left > self.encoder_high_wrap) :
+                self.l_wheel_mult = self.l_wheel_mult + 1     
+            elif (left_enc > self.encoder_high_wrap and self.enc_left < self.encoder_low_wrap) :
+                self.l_wheel_mult = self.l_wheel_mult - 1
             else:
-                if (left_enc < self.encoder_low_wrap and self.enc_left > self.encoder_high_wrap) :
-                    self.l_wheel_mult = self.l_wheel_mult + 1     
-                elif (left_enc > self.encoder_high_wrap and self.enc_left < self.encoder_low_wrap) :
-                    self.l_wheel_mult = self.l_wheel_mult - 1
-                else:
-                     self.l_wheel_mult = 0
-                if (right_enc < self.encoder_low_wrap and self.enc_right > self.encoder_high_wrap) :
-                    self.r_wheel_mult = self.r_wheel_mult + 1     
-                elif (right_enc > self.encoder_high_wrap and self.enc_right < self.encoder_low_wrap) :
-                    self.r_wheel_mult = self.r_wheel_mult - 1
-                else:
-                     self.r_wheel_mult = 0
-                #dright = (right_enc - self.enc_right) / self.ticks_per_meter
-                #dleft = (left_enc - self.enc_left) / self.ticks_per_meter
-                dleft = 1.0 * (left_enc + self.l_wheel_mult * (self.encoder_max - self.encoder_min)-self.enc_left) / self.ticks_per_meter 
-                dright = 1.0 * (right_enc + self.r_wheel_mult * (self.encoder_max - self.encoder_min)-self.enc_right) / self.ticks_per_meter 
+                    self.l_wheel_mult = 0
+            if (right_enc < self.encoder_low_wrap and self.enc_right > self.encoder_high_wrap) :
+                self.r_wheel_mult = self.r_wheel_mult + 1     
+            elif (right_enc > self.encoder_high_wrap and self.enc_right < self.encoder_low_wrap) :
+                self.r_wheel_mult = self.r_wheel_mult - 1
+            else:
+                    self.r_wheel_mult = 0
+            #dright = (right_enc - self.enc_right) / self.ticks_per_meter
+            #dleft = (left_enc - self.enc_left) / self.ticks_per_meter
+            dleft = 1.0 * (left_enc + self.l_wheel_mult * (self.encoder_max - self.encoder_min)-self.enc_left) / self.ticks_per_meter 
+            dright = 1.0 * (right_enc + self.r_wheel_mult * (self.encoder_max - self.encoder_min)-self.enc_right) / self.ticks_per_meter 
 
-            self.enc_right = right_enc
-            self.enc_left = left_enc
+        self.enc_right = right_enc
+        self.enc_left = left_enc
+        
+        dxy_ave = (dright + dleft) / 2.0
+        dth = (dright - dleft) / self.wheel_track
+        vxy = dxy_ave / dt
+        vth = dth / dt
             
-            dxy_ave = (dright + dleft) / 2.0
-            dth = (dright - dleft) / self.wheel_track
-            vxy = dxy_ave / dt
-            vth = dth / dt
-                
-            if (dxy_ave != 0):
-                dx = cos(dth) * dxy_ave
-                dy = -sin(dth) * dxy_ave
-                self.x += (cos(self.th) * dx - sin(self.th) * dy)
-                self.y += (sin(self.th) * dx + cos(self.th) * dy)
-    
-            if (dth != 0):
-                self.th += dth 
-    
-            quaternion = Quaternion()
-            quaternion.x = 0.0 
-            quaternion.y = 0.0
-            quaternion.z = sin(self.th / 2.0)
-            quaternion.w = cos(self.th / 2.0)
-    
-            # Create the odometry transform frame broadcaster.
-            if (self.useImu == False) :
-                t = TransformStamped()
+        if (dxy_ave != 0):
+            dx = cos(dth) * dxy_ave
+            dy = -sin(dth) * dxy_ave
+            self.x += (cos(self.th) * dx - sin(self.th) * dy)
+            self.y += (sin(self.th) * dx + cos(self.th) * dy)
 
-                t.header.stamp = self.get_clock().now().to_msg()
-                t.transform.translation.x = self.x
-                t.transform.translation.y = self.y
-                t.transform.translation.z = 0.0
-                t.transform.rotation.x = quaternion.x
-                t.transform.rotation.y = quaternion.y
-                t.transform.rotation.z = quaternion.z
-                t.transform.rotation.w = quaternion.w
-                t.child_frame_id = self.base_frame
-                t.header.frame_id = 'odom'
+        if (dth != 0):
+            self.th += dth 
 
-                self.odomBroadcaster.sendTransform(t)
-    
-            odom = Odometry()
-            odom.header.frame_id = "odom"
-            odom.child_frame_id = self.base_frame
-            odom.header.stamp = self.get_clock().now().to_msg()
-            odom.pose.pose.position.x = self.x
-            odom.pose.pose.position.y = self.y
-            odom.pose.pose.position.z = 0
-            odom.pose.pose.orientation = quaternion
-            odom.twist.twist.linear.x = vxy
-            odom.twist.twist.linear.y = 0
-            odom.twist.twist.angular.z = vth
+        quaternion = Quaternion()
+        quaternion.x = 0.0 
+        quaternion.y = 0.0
+        quaternion.z = sin(self.th / 2.0)
+        quaternion.w = cos(self.th / 2.0)
 
-            odom.pose.covariance = ODOM_POSE_COVARIANCE
-            odom.twist.covariance = ODOM_TWIST_COVARIANCE
+        # Create the odometry transform frame broadcaster.
+        if (self.useImu == False) :
+            t = TransformStamped()
 
-            self.odomPub.publish(odom)
+            t.header.stamp = self.get_clock().now().to_msg()
+            #t.transform.translation.x = float(self.x) if self.x > 0.02 else 0.0
+            t.transform.translation.x = float(self.x)
+            t.transform.translation.y = float(self.y)
+            t.transform.translation.z = 0.0
+            t.transform.rotation.x = quaternion.x
+            t.transform.rotation.y = quaternion.y
+            t.transform.rotation.z = quaternion.z
+            t.transform.rotation.w = quaternion.w
+            t.child_frame_id = self.base_frame
+            t.header.frame_id = 'odom'
+
+            self.odomBroadcaster.sendTransform(t)
+
+        odom = Odometry()
+        #odom.header.frame_id = "odom"
+        odom.header.frame_id = "dashgo_odom"
+        odom.child_frame_id = self.base_frame
+        odom.header.stamp = self.get_clock().now().to_msg()
+        odom.pose.pose.position.x = float(self.x)
+        odom.pose.pose.position.y = float(self.y)
+        odom.pose.pose.position.z = 0.0
+        odom.pose.pose.orientation = quaternion
+        odom.twist.twist.linear.x = vxy
+        odom.twist.twist.linear.y = 0.0
+        odom.twist.twist.angular.z = vth
+
+        #odom.pose.covariance = ODOM_POSE_COVARIANCE
+        #odom.twist.covariance = ODOM_TWIST_COVARIANCE
+        odom.pose.covariance = ODOM_POSE_COVARIANCE2
+        odom.twist.covariance = ODOM_TWIST_COVARIANCE2
+
+        self.odomPub.publish(odom)
+        
+        if self.now > (self.last_cmd_vel + Duration(seconds=self.timeout)):
+            self.v_des_left = 0
+            self.v_des_right = 0
             
-            if self.now > (self.last_cmd_vel + rclpy.duration.Duration(self.timeout)):
-                self.v_des_left = 0
-                self.v_des_right = 0
-                
+        if self.v_left < self.v_des_left:
+            self.v_left += self.max_accel
+            if self.v_left > self.v_des_left:
+                self.v_left = self.v_des_left
+        else:
+            self.v_left -= self.max_accel
             if self.v_left < self.v_des_left:
-                self.v_left += self.max_accel
-                if self.v_left > self.v_des_left:
-                    self.v_left = self.v_des_left
-            else:
-                self.v_left -= self.max_accel
-                if self.v_left < self.v_des_left:
-                    self.v_left = self.v_des_left
-            
+                self.v_left = self.v_des_left
+        
+        if self.v_right < self.v_des_right:
+            self.v_right += self.max_accel
+            if self.v_right > self.v_des_right:
+                self.v_right = self.v_des_right
+        else:
+            self.v_right -= self.max_accel
             if self.v_right < self.v_des_right:
-                self.v_right += self.max_accel
-                if self.v_right > self.v_des_right:
-                    self.v_right = self.v_des_right
-            else:
-                self.v_right -= self.max_accel
-                if self.v_right < self.v_des_right:
-                    self.v_right = self.v_des_right
-            self.lVelPub.publish(self.v_left)
-            self.rVelPub.publish(self.v_right)            
+                self.v_right = self.v_des_right
+                  
 
-            # Set motor speeds in encoder ticks per PID loop
-            #if not self.stopped:
-            if ((not self.stopped) and (not self.is_recharge)):
-                self.controller.drive(self.v_left, self.v_right)
-    
+        # Set motor speeds in encoder ticks per PID loop
+        #if not self.stopped:
+        if ((not self.stopped)):
+            # print(f"vel l {self.v_left} and vel r {self.v_right}")
+            self.controller.drive(int(self.v_left), int(self.v_right))
 
-        def cmdVelCallback(self, req):
+
+    def cmdVelCallback(self, req):
             self.last_cmd_vel = self.get_clock().now()
             x = req.linear.x         # m/s
             th = req.angular.z
