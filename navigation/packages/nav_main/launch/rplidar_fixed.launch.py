11c0ed5df8a91862405614152ec8241ca4cff6ef
GerardoFJ <A00838645@tec.mx>
2025-03-01 03:35:30 -0600

    created nav_basics

--- navigation/packages/nav_main/launch/rplidar_fixed.launch.py
+++ navigation/packages/nav_main/launch/rplidar_fixed.launch.py
@@ -1,43 +1,6 @@
-# from launch_ros.substitutions import FindPackageShare
-# from launch import LaunchDescription
-# from launch.actions import IncludeLaunchDescription, Node
-# from launch.launch_description_sources import PythonLaunchDescriptionSource
-# from launch.substitutions import PathJoinSubstitution, TextSubstitution
-
-# def generate_launch_description():
-
-#     return LaunchDescription([
-#         # IncludeLaunchDescription(
-#         #     PythonLaunchDescriptionSource([
-#         #         PathJoinSubstitution([
-#         #             FindPackageShare('sllidar_ros2'),
-#         #             'launch',
-#         #             'sllidar_a1.launch.py'
-#         #         ])
-#         #     ]),
-#         #     launch_arguments={
-#         #         'serial_port': '/dev/ttyUSB0',
-#         #         'frame_id': 'laser',
-#         #         'inverted':'true',
-#         #     }.items()
-#         # ),
-#         Node(
-#             package='tf2_ros',
-#             executable='static_transform_publisher',
-#             name='laser_to_base_link',
-#             parameters=[
-#                 "0.1", "0.0", "0.2", "-1.57", "0.0", "0.0", "/base_link", "/laser", "40"
-#             ],
-#         ),
-#     ])
-
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     return LaunchDescription([
@@ -62,5 +25,3 @@ def generate_launch_description():
             ),
 
     ])
-
-#<param name="ignore_array" type="string" value="-176 ,-166, -129,-119, -85, -67, -56, -46, -10,5" />
