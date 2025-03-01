d792d6bc8983e6958e11e8fc6f9e046e6b5bdcbe
IvanRomero03 <iwells.ar@gmail.com>
2025-03-01 13:28:46 -0600

    add down sample for pointcloud

---
+++ manipulation/packages/perception_3d/src/publish_pcl.cpp
@@ -0,0 +1,116 @@
+#include <chrono>
+#include <climits>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/impl/point_types.hpp>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <perception_3d/macros.hpp>
+#include <rclcpp/logging.hpp>
+#include <rclcpp/timer.hpp>
+#include <sensor_msgs/msg/detail/point_cloud2__struct.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <string>
+#include <tf2_ros/transform_broadcaster.h>
+#include <variant>
+
+// #include <tf2_ros/static_transform_broadcaster.hpp>
+// #include <frida_interfaces/action/detect_objects3_d.hpp>
+
+#include "rclcpp/rclcpp.hpp"
+#include <frida_interfaces/srv/remove_plane.hpp>
+#include <frida_interfaces/srv/test.hpp>
+
+using namespace std::chrono_literals;
+
+class PublishNode : public rclcpp::Node {
+private:
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+  const std::string cloud_file =
+      "/home/ivanromero/Desktop/home2/manipulation/"
+      "packages/perception_3d/pcl_debug/pcl_table.pcd";
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher;
+  rclcpp::TimerBase::SharedPtr timer;
+  rclcpp::TimerBase::SharedPtr timer_2;
+
+public:
+  PublishNode() : Node("publish_node") {
+    RCLCPP_INFO(this->get_logger(), "Starting Publish Node");
+
+    cloud =
+        pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
+
+    // load point cloud
+
+    if (pcl::io::loadPCDFile(cloud_file, *cloud) == -1) {
+      PCL_ERROR("Couldn't read file %s \n", cloud_file.c_str());
+      return;
+    }
+
+    RCLCPP_INFO(this->get_logger(), "Loaded point cloud");
+
+    auto qos = rclcpp::QoS(rclcpp::SensorDataQoS());
+    qos.reliability(rclcpp::ReliabilityPolicy::Reliable);
+    publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+        "/point_cloud", qos);
+
+    RCLCPP_INFO(this->get_logger(), "Created publisher");
+
+    this->timer =
+        this->create_wall_timer(100ms, std::bind(&PublishNode::loop, this));
+
+    RCLCPP_INFO(this->get_logger(), "Created timer");
+    this->timer_2 = this->create_wall_timer(
+        10ms, std::bind(&PublishNode::tf_publisher, this));
+  }
+
+  void loop() {
+    auto msg = sensor_msgs::msg::PointCloud2();
+    pcl::toROSMsg(*cloud, msg);
+    // msg.header.frame_id = "zed_left_camera_frame";
+    msg.header.frame_id = "base_link";
+    publisher->publish(msg);
+  }
+
+  void tf_publisher() {
+    int x = 0;
+    int y = 0;
+    int z = 0;
+
+    auto base_link = std::make_shared<tf2_ros::TransformBroadcaster>(
+        this->shared_from_this());
+
+    geometry_msgs::msg::TransformStamped transform_stamped;
+    transform_stamped.header.stamp = this->now();
+    transform_stamped.header.frame_id = "base_link";
+    transform_stamped.transform.translation.x = x;
+    transform_stamped.transform.translation.y = y;
+    transform_stamped.transform.translation.z = z;
+    transform_stamped.transform.rotation.x = 0;
+    transform_stamped.transform.rotation.y = 0;
+    transform_stamped.transform.rotation.z = 0;
+    transform_stamped.transform.rotation.w = 1;
+    transform_stamped.child_frame_id = "table";
+
+    base_link->sendTransform(transform_stamped);
+  }
+};
+
+int main(int argc, _IN_ char *argv[]) {
+  rclcpp::init(argc, argv);
+
+  RCLCPP_INFO(rclcpp::get_logger("table_segmentation_main"),
+              "Starting Table Segmentation Node");
+
+  rclcpp::spin(std::make_shared<PublishNode>());
+
+  rclcpp::shutdown();
+
+  return 0;
+}
