3a026956a9e60ca864b10d16ab173e42c6a10a3a
GerardoFJ <A00838645@tec.mx>
2025-03-01 07:35:57 -0600

    Fixed nav_basics

--- navigation/packages/nav_main/launch/nav_basics.launch.py
+++ navigation/packages/nav_main/launch/nav_basics.launch.py
@@ -2,13 +2,21 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
 
 def generate_launch_description():
-    publish_urdf = LaunchConfiguration('publish_tf', default=True)
+    publish_urdf = LaunchConfiguration('publish_tf')
+
+    # Declare the launch argument
+    declare_publish_tf = DeclareLaunchArgument(
+        'publish_tf',
+        default_value='true',  # LaunchConfiguration values are strings, so use 'true'/'false'
+        description='Whether to publish URDF'
+    )
 
     dashgo_driver = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -31,7 +39,6 @@ def generate_launch_description():
             )
         ))
     
-
     robot_description_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution(
@@ -46,6 +53,7 @@ def generate_launch_description():
             'urdf_package': 'frida_description',
             'urdf_package_path': PathJoinSubstitution(['urdf','FRIDA_Real.urdf.xacro'])
         }.items(),
+        condition=IfCondition(publish_urdf),  # Condition to include this launch file only if publish_tf is true
     )
 
     laser_launch = IncludeLaunchDescription(
@@ -62,19 +70,14 @@ def generate_launch_description():
     joint_state = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
+        condition=IfCondition(publish_urdf),  # Only launch joint_state if publish_tf is true
     )
 
-    if publish_urdf:
-        return LaunchDescription([
-            dashgo_driver,
-            ekf_launch,
-            robot_description_launch,
-            joint_state,
-            laser_launch
-        ])
-    else:
-        return LaunchDescription([
-            dashgo_driver,
-            ekf_launch,
-            laser_launch
-        ])
+    return LaunchDescription([
+        declare_publish_tf,  # Declare launch argument
+        dashgo_driver,
+        ekf_launch,
+        robot_description_launch,
+        joint_state,
+        laser_launch
+    ])
