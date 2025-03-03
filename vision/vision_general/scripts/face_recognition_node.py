f05eda920d976a1e1a3102da63af702c3aa74bf9
Alejandra Coeto <109093534+Ale-Coeto@users.noreply.github.com>
2025-03-03 00:08:31 -0600

    Update vision/vision_general/scripts/face_recognition_node.py
    Co-authored-by: Iván Romero <i.wells.ar@gmail.com>

--- vision/vision_general/scripts/face_recognition_node.py
+++ vision/vision_general/scripts/face_recognition_node.py
@@ -360,7 +360,7 @@ class FaceRecognition(Node):
         # Calculate the joint degrees for the arm to follow the face
         if detected:
             self.publish_follow_face(xc, yc, largest_face_name)
-        if self.get_parameter("verbose").value:
+        if self.verbose:
             cv2.imshow("Face detection", annotated_frame)
         self.image_view = annotated_frame
         self.view_pub.publish(self.bridge.cv2_to_imgmsg(annotated_frame, "bgr8"))
