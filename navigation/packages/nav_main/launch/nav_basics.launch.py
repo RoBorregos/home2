732b5270b930680c54612187469c88e7eb74ca8e
Gerardo Fregoso Jiménez <143139229+GerardoFJ@users.noreply.github.com>
2025-03-06 11:10:21 -0600

    Moveit config insano (#115)
    * insano as fuck
    
    * El diablooo
    
    * Fixed lidar el diablooo
    
    * Fixed xacro insano
    
    * SRDF update but not probe xddd
    
    * Fixeada la chingadera del oscar
    
    * Added zed fixed
    
    * Changed base_link
    
    * created nav_basics
    
    ---------
    
    Co-authored-by: lleo-13 <a00830708@tec.mx>

---
+++ navigation/packages/nav_main/launch/nav_basics.launch.py
@@ -0,0 +1,73 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    publish_urdf = LaunchConfiguration('publish_tf', default=True)
+
+    dashgo_driver = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("dashgo_driver"),
+                    "launch",
+                    "dashgo_driver.launch.py",
+                ]
+            )
+        ))
+    ekf_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("dashgo_driver"),
+                    "launch",
+                    "ekf.launch.py",
+                ]
+            )
+        ))
+    
+
+    robot_description_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("urdf_launch"),
+                    "launch",
+                    "description.launch.py",
+                ]
+            )
+        ),
+        launch_arguments={
+            'urdf_package': 'frida_description',
+            'urdf_package_path': PathJoinSubstitution(['urdf','FRIDA_Real.urdf.xacro'])
+        }.items(),
+    )
+
+    laser_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("nav_main"),
+                    "launch",
+                    "rplidar_fixed.launch.py",
+                ]
+            )
+        ))
+    if publish_urdf:
+        return LaunchDescription([
+            dashgo_driver,
+            ekf_launch,
+            robot_description_launch,
+            laser_launch
+        ])
+    else:
+        return LaunchDescription([
+            dashgo_driver,
+            ekf_launch,
+            laser_launch
+        ])
