5d5c6e173cc81c47425a5004938b55a5752ccde2
Emiliano Flores <114429692+EmilianoHFlores@users.noreply.github.com>
2025-03-08 12:40:42 -0600

    Demo with dualshock 4 (#122)
    * dualshock demo gripper + joint goals
    
    * Added manipulation constants and changed print to node loggers

---
+++ frida_constants/frida_constants/manipulation_constants.py
@@ -0,0 +1,9 @@
+from math import pi as PI
+
+DEG2RAD = PI / 180.0
+RAD2DEG = 180.0 / PI
+# xArm Constants
+XARM_SETMODE_SERVICE = "/xarm/set_mode"
+XARM_SETSTATE_SERVICE = "/xarm/set_state"
+MOVEIT_MODE = 1
+JOINTN_VELOCITY_MODE = 4
