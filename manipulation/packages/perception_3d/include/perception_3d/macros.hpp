17ede7b2579842f94bede9d2d2a4946de40f5488
Iván Romero <i.wells.ar@gmail.com>
2025-03-12 23:17:07 -0600

    Cluster object (#155)
    * tests
    
    * done
    
    * added comments and docs. Added stampped point and add cluster to output srv
    
    ---------
    
    Co-authored-by: IvanRomero03 <iwells.ar@gmail.com>

--- manipulation/packages/perception_3d/include/perception_3d/macros.hpp
+++ manipulation/packages/perception_3d/include/perception_3d/macros.hpp
@@ -3,16 +3,17 @@
 #define _IN_
 #define _OUT_
 
-// u32b status error codes
-// #define OK 0
-// #define COULD_NOT_CONVERT_POINT_CLOUD 1
-
 enum u32bStatusErrorCodes {
   OK = 0,
   COULD_NOT_CONVERT_POINT_CLOUD = 1,
   COULD_NOT_ESTIMATE_PLANAR_MODEL = 2,
   COULD_NOT_SAVE_POINT_CLOUD = 3,
   NO_POINTCLOUD = 4,
+  INVALID_INPUT_FILTER = 5,
+  NO_POINT_DETECTED = 6,
+  NO_OBJECT_TO_CLUSTER_AT_POINT = 7,
+  POINT_CLOUD_EMPTY = 8,
+  COULDNT_TRANSFORM_TO_BASE_LINK = 9,
 };
 
 #define ASSERT(var, val, message)                                              \
@@ -29,6 +30,12 @@ enum u32bStatusErrorCodes {
   if (var != val) {                                                            \
     RCLCPP_ERROR(this->get_logger(), message, code);                           \
     return;                                                                    \
+  }
+
+#define ASSERT_AND_RETURN_CODE_VALUE(var, val, message, code)                  \
+  if (var != val) {                                                            \
+    RCLCPP_ERROR(this->get_logger(), message, code);                           \
+    return code;                                                               \
   }
 
 #endif
