8c6a0dfc25a5e5c8c8e88d68b9eca61e6b502e77
EmilianoHFlores <A00833160@tec.mx>
2025-03-14 20:53:10 -0600

    Defined sets of joints and added move single joint example for receptionist manager

---
+++ frida_constants/frida_constants/xarm_configurations.py
@@ -0,0 +1,12 @@
+FRONT_STARE = {
+    "joint1": -90.0,
+    "joint2": -45.0,
+    "joint3": -90.0,
+    "joint4": 0.0,
+    "joint5": 40.0,
+    "joint6": 70.0,
+}
+
+XARM_CONFIGURATIONS = {
+    "front_stare": FRONT_STARE,
+}
