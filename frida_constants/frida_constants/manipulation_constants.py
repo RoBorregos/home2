42ee4971a20d181c98962efc487c1a3686958d25
Emiliano Flores <114429692+EmilianoHFlores@users.noreply.github.com>
2025-03-15 19:50:58 -0500

    Update receptionist adds to pick branch (#169)
    * Defined sets of joints and added move single joint example for receptionist manager
    
    * updated configuration
    
    * Added service to move by joint velocities

--- frida_constants/frida_constants/manipulation_constants.py
+++ frida_constants/frida_constants/manipulation_constants.py
@@ -10,6 +10,12 @@ RAD2DEG = 180.0 / PI
 XARM_SETMODE_SERVICE = "/xarm/set_mode"
 XARM_SETSTATE_SERVICE = "/xarm/set_state"
 MOVEIT_MODE = 1
+JOINT_VELOCITY_MODE = 4
+
+XARM_MOVEVELOCITY_SERVICE = "/xarm/vc_set_joint_velocity"
+SET_JOINT_VELOCITY_SERVICE = "/manipulation/set_joint_velocity"
+
+ALWAYS_SET_MODE = True
 JOINTN_VELOCITY_MODE = 4
 
 MOVE_JOINTS_ACTION_SERVER = "/manipulation/move_joints_action_server"
