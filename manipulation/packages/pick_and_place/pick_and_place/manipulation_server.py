ff2829d8a95f91454e4c80944b97f1513ff108e2
Iván Romero <i.wells.ar@gmail.com>
2025-03-15 19:10:04 -0600

    asdasdqweqweqwasdqwdqwdqwdqwdqsqdqdqdqdqdqdqdqdqdqd

--- manipulation/packages/pick_and_place/pick_and_place/manipulation_server.py
+++ manipulation/packages/pick_and_place/pick_and_place/manipulation_server.py
@@ -12,6 +12,8 @@ from frida_constants.manipulation_constants import (
     PICK_ACTION_SERVER,
 )
 from frida_interfaces.action import PickTask
+from geometry_msgs.msg import Point
+from tf2_ros import Buffer, TransformListener
 
 
 class ManipulationServer(Node):
@@ -27,8 +29,48 @@ class ManipulationServer(Node):
         )
 
         # Server here, which is the interface between manipulation tasks and the task managers
-
         # Point Subscriber for debug here
+        self.subs = self.create_subscription(
+            Point,
+            "/clicked_point",
+            self.point_callback,
+            10,
+        )
+        self.last_point = None
+        self.last_process_point = None
+
+        self.get_logger().info("Manipulation Server is ready")
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self, True)
+
+        self.create_timer(0.1, self.timer_callback)
+
+    def point_callback(self, msg):
+        self.last_point = msg
+
+    def feedback_callback(self, feedback):
+        self.get_logger().info(f"Feedback: {feedback}")
+
+    def result_callback(self, result):
+        self.get_logger().info(f"Result: {result}")
+
+    def timer_callback(self):
+        if self.last_point == self.last_process_point:
+            return
+        self.last_process_point = self.last_point
+        if self.last_point is None:
+            return
+        a = PickTask.Goal()
+        a.object_point = self.last_point
+        a.object_name = "Clicked Point"
+        self._pick_action_client.send_goal_async(
+            a,
+            feedback_callback=self.feedback_callback,
+            result_callback=self.result_callback,
+        )
+        self.get_logger().info(f"Received point: {self.last_point}")
+        self.get_logger().info("Sending to manipulation core")
 
 
 def main(args=None):
