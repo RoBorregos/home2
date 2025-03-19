989034ee140fac78fe5547c8424ab2b12da295fc
Alexis Chapa <93449818+Chapa-1810@users.noreply.github.com>
2025-03-18 21:35:20 -0600

    Working object detector 2d node (#175)
    * started msg in frida interfaces}
    
    * add geometry_msgs and frida_interfaces dependencies to CMakeLists and package.xml; update import paths in Detection2D.py
    
    * Added first iteration for NEW object detector 2d
    
    * started msg in frida interfaces}
    
    * add geometry_msgs and frida_interfaces dependencies to CMakeLists and package.xml; update import paths in Detection2D.py
    
    * Added first iteration for NEW object detector 2d
    
    * Small fixes for compile code
    
    * local_camera_node
    
    * Working Object Detector 2D with YoloV5
    
    * Testing tf version
    
    * .
    
    * Stable version of object detector 2D
    
    * Added imutils to requirements.txt
    
    * Moved Object Detector 2D to vision folder, removed deprecated 2D object detector. FIxed ruffus errors
    
    * .
    
    ---------
    Authored-by: Alexis Chapa Romero <alexischaparomero@gmail.com>
    Co-authored-by: AleGonzcamilla <A00839233@tec.mx>
    Co-authored-by: José Luis Domínguez Morales <jose.dominguez@oneila.mx>

---
+++ vision/object_detector_2d/scripts/YoloV5ObjectDetector.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+from ObjectDetector import ObjectDectector, Detection, ObjectDectectorParams
+import torch
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+class YoloV5ObjectDetector(ObjectDectector):
+    def __init__(self, model_path: str, object_detector_params: ObjectDectectorParams):
+        super().__init__(model_path, object_detector_params)
+        self.loadYoloV5Model()
+
+    def loadYoloV5Model(self):
+        self.model = torch.hub.load(
+            "ultralytics/yolov5",
+            "custom",
+            path=self.model_path_,
+            force_reload=False,
+        )
+
+    def _inference(self, frame):
+        detections = self.model(frame)
+
+        for *xyxy, confidence, _class, names in (
+            detections.pandas().xyxy[0].itertuples(index=False)
+        ):
+            # Normalized [0-1] ymin, xmin, ymax, xmax
+            height = frame.shape[1]
+            width = frame.shape[0]
+            if confidence < self.object_detector_params_.min_score_thresh:
+                continue
+
+            # print(f"Detection: {names}, class: {_class}, confidence: {confidence}, xyxy: {xyxy}, height: {height}, width: {width}")
+
+            detection_ = Detection(names, _class, confidence)
+            detection_.bbox_.h, detection_.bbox_.w = height, width
+            detection_.bbox_.y1 = xyxy[1] / width
+            detection_.bbox_.x1 = xyxy[0] / height
+            detection_.bbox_.y2 = xyxy[3] / width
+            detection_.bbox_.x2 = xyxy[2] / height
+            self.detections_.append(detection_)
+
+        return self.detections_
