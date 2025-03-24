5bc5c31b5997b29e7f97851de6fe25909bee211a
Chapa-1810 <alexischaparomero@gmail.com>
2025-03-24 19:42:14 +0000

    .

--- vision/object_detector_2d/launch/object_detector_node.local.launch.py
+++ vision/object_detector_2d/launch/object_detector_node.local.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
                 respawn=True,
                 output="screen",
                 emulate_tty=True,
-                parameters=[config],
+                parameters=[config],    
             ),
         ]
     )
