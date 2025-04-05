f5d866932741bba36748651516032c223965d720
EmilianoHFlores <a00833160@tec.mx>
2025-04-05 11:02:52 -0600

    fixes to detector node constants and added template for depth camera with kinect plugin

--- frida_constants/frida_constants/vision_constants.py
+++ frida_constants/frida_constants/vision_constants.py
@@ -1,14 +1,14 @@
 # ZED CAMERA TOPICS
 CAMERA_TOPIC = "/zed/zed_node/rgb/image_rect_color"
-DEPTH_IMAGE_TOPIC = " /zed/zed_node/depth/depth_registered"
-CAMERA_INFO_TOPIC = " /zed/zed_node/depth/camera_info"
+DEPTH_IMAGE_TOPIC = "/zed/zed_node/depth/depth_registered"
+CAMERA_INFO_TOPIC = "/zed/zed_node/depth/camera_info"
 
 # ZED CAMERA FRAME
 CAMERA_FRAME = "zed_left_camera_optical_frame"
 
 # OBJECT 2D TOPICS
 DETECTIONS_TOPIC = "/vision/detections"
-DETECTIONS_IMAGE_TOPIC = "//visiondetections_image"
+DETECTIONS_IMAGE_TOPIC = "/vision/detections_image"
 DETECTIONS_POSES_TOPIC = "/vision/detection_poses"
 DETECTIONS_3D_TOPIC = "/vision/detections_3d"
 DETECTIONS_ACTIVE_TOPIC = "/detections_active"
