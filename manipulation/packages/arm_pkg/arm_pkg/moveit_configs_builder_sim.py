79446ec062c6a274d0c3882313bf90e8dc83f25d
EmilianoHFlores <a00833160@tec.mx>
2025-04-05 01:44:42 -0600

    Added zed and Roborregos gripper simulation using realsense plugin

--- manipulation/packages/arm_pkg/arm_pkg/moveit_configs_builder_sim.py
+++ manipulation/packages/arm_pkg/arm_pkg/moveit_configs_builder_sim.py
@@ -271,6 +271,7 @@ class MoveItConfigsBuilder(ParameterBuilder):
         geometry_mesh_origin_rpy = get_list_param_str('geometry_mesh_origin_rpy', '0 0 0')
         geometry_mesh_tcp_xyz = get_list_param_str('geometry_mesh_tcp_xyz', '0 0 0')
         geometry_mesh_tcp_rpy = get_list_param_str('geometry_mesh_tcp_rpy', '0 0 0')
+        load_zed = get_param_str('load_zed', False)
 
         self.__prefix = prefix
         self.__robot_dof = dof
@@ -318,6 +319,8 @@ class MoveItConfigsBuilder(ParameterBuilder):
             'geometry_mesh_origin_rpy': geometry_mesh_origin_rpy,
             'geometry_mesh_tcp_xyz': geometry_mesh_tcp_xyz,
             'geometry_mesh_tcp_rpy': geometry_mesh_tcp_rpy,
+            'load_zed': load_zed,
+            'simulation': "True",
         }
         self.__srdf_xacro_args = {
             'prefix': prefix,
