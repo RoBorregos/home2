9a22841f86010934b5ff16818f1ad3ebb749e7d0
Alejandra Coeto <109093534+Ale-Coeto@users.noreply.github.com>
2025-04-11 22:02:37 -0600

    Vision subtask (#252)
    * Update vision task manager for moondream queries
    
    * update vision constants
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Update vision_tests
    
    * Help me carry
    
    * Help me carry
    
    * Help me carry
    
    * Help me carry
    
    * Help me carry
    
    * Help me carry
    
    * Help me carry
    
    * Help me carry
    
    * Update test visio
    
    * Fix ruff
    
    * Testing
    
    * Tests
    
    * fix ruff

---
+++ task_manager/scripts/test_vision_manager.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+"""
+Task Manager for testing the subtask managers
+"""
+
+import rclpy
+from rclpy.node import Node
+
+from subtask_managers.vision_tasks import VisionTasks
+from utils.task import Task
+from utils.logger import Logger
+
+task = Task.DEBUG
+
+
+class TestVision(Node):
+    def __init__(self):
+        super().__init__("test_task_manager")
+        self.manager = VisionTasks(self, task=task, mock_data=False)
+        self.get_logger().info("TestTaskManager has started.")
+        self.running_task = True
+        self.response = "test"
+        self.done = False
+
+    def setResponse(self, status, response):
+        self.response = response
+        print("RECEIVED RESPONSE")
+
+    def run(self):
+        if task == Task.DEBUG:
+            if not self.done:
+                self.manager.describe_person(self.setResponse)
+                self.done = True
+
+            noasync = self.manager.moondream_query("is there anyone in the image?", False)
+            Logger.info(self, f"Vision task result: {noasync}")
+            # status, description = self.manager.describe_bag([0, 0, 1, 1])
+            # print(description)
+
+            if self.response != "test":
+                self.get_logger().info(f"Vision task result: {self.response}")
+                self.done = True
+                self.running_task = False
+
+
+def main(args=None):
+    """Main function"""
+    rclpy.init(args=args)
+    node = TestVision()
+
+    try:
+        while rclpy.ok() and node.running_task:
+            rclpy.spin_once(node, timeout_sec=0.1)
+            node.run()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- task_manager/scripts/utils/task.py
+++ task_manager/scripts/utils/task.py
@@ -5,3 +5,6 @@ class Task(Enum):
     DEMO = 0
     RECEPTIONIST = 1
     HELP_ME_CARRY = 2
+    GPSR = 3
+    STORING_GROCERIES = 4
+    DEBUG = 5
