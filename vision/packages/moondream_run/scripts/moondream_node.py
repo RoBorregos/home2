85edf679ce67251f10be80346c27404f94bdf373
Alejandra Coeto <a01285221@tec.mx>
2025-04-14 13:13:18 -0600

    Fix crop query

--- vision/packages/moondream_run/scripts/moondream_node.py
+++ vision/packages/moondream_run/scripts/moondream_node.py
@@ -153,10 +153,10 @@ class MoondreamNode(Node):
         ymax = ymax * self.image.shape[0]
 
         if (
-            xmin < 0
-            or ymin < 0
-            or xmax > self.image.shape[1]
-            or ymax > self.image.shape[0]
+            xmin >= 0
+            or ymin >= 0
+            or xmax < self.image.shape[1]
+            or ymax < self.image.shape[0]
         ):
             self.image = self.image[int(ymin) : int(ymax), int(xmin) : int(xmax)]
 
