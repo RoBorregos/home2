2526e777adf8c5d9c9c4f0a968de275dd61d4e1c
david_vl2003@hotmail.com <david_vl2003@hotmail.com>
2025-04-15 17:42:26 -0600

    Removed follow person function temporary

--- task_manager/scripts/subtask_managers/generic_tasks.py
+++ task_manager/scripts/subtask_managers/generic_tasks.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 from utils.subtask_manager import SubtaskManager
-import math as m
-import numpy as np
+
 
 class GenericTask:
     """Class to manage the generic tasks"""
@@ -38,24 +37,26 @@ class GenericTask:
         i = 0
         while i < 5:
             print("Hello, I am a generic task manager.")
-            self.subtask.hri.say("Hello, I am a generic task manager.")
+            self.subtask.hri.say(self.subtask.hri.node.test_text)
             i += 1
             import time
 
             time.sleep(1)
         return True
-    
-    def follow_person(self):
-        KP = 0.5
-        while True:
-            dashgo_cmd_vel = self.subtask.nav.node.cmd_vel
-            dashgo_rotation_vel = np.sign(dashgo_cmd_vel.angular.z)
-            
-            # base_rotational_velocitiy_sign = np.sign(cmd_vel_subscriptor_value)
-            # arm_rotational_velocity = person_bounding_box_center_x * KP
-            # arm_rotation_velocity_sign = np.sign(arm_rotational_velocity)
-            
-            # person_pose = self.subtask.vision.detect_person()
-            # person_camera_point = self.subtask.vision.get_person_camera_point()
-            # self.subtask.nav.follow_person(True,person_pose)
-            # self.subtask.manipulation.pan_to()
+
+    # def follow_person(self):
+    #     KP = 0.5
+    #     while True:
+    #         break
+    #         # dashgo_cmd_vel = self.subtask.nav.cmd_vel
+    #         # dashgo_rotation_vel_sign = np.sign(dashgo_cmd_vel.angular.z)
+    #         # person_boinding_box_center = self.vision.detect_person()
+
+    #         # base_rotational_velocitiy_sign = np.sign(cmd_vel_subscriptor_value)
+    #         # arm_rotational_velocity = person_bounding_box_center_x * KP
+    #         # arm_rotation_velocity_sign = np.sign(arm_rotational_velocity)
+
+    #         # person_pose = self.subtask.vision.detect_person()
+    #         # person_camera_point = self.subtask.vision.get_person_camera_point()
+    #         # self.subtask.nav.follow_person(True,person_pose)
+    #         # self.subtask.manipulation.pan_to()
