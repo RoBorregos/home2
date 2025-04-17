441b770074e72a0316d9d54a1d2b4098a531db63
Oscar Arreola <77394565+Oscar-gg@users.noreply.github.com>
2025-04-16 20:11:16 -0600

    Gpsr function implementation (#264)
    * feat(gpsr): function signatures added
    
    * fix: hri subtask
    
    * refactor: gpsr task manager
    
    * feat: added single area connector to gpsr
    
    * feat: mock command interpreter

--- task_manager/scripts/utils/subtask_manager.py
+++ task_manager/scripts/utils/subtask_manager.py
@@ -12,7 +12,7 @@ class SubtaskManager:
     def __init__(self, node, task: Task, mock_areas: list[str] = []):
         self.vision = VisionTasks(node, task=task, mock_data=("vision" in mock_areas))
         hri_config = mocked_hri_config if "hri" in mock_areas else test_hri_config
-        self.hri = HRITasks(node, config=hri_config)
+        self.hri = HRITasks(node, config=hri_config, task=task)
 
         self.manipulation = ManipulationTasks(
             node, task=task, mock_data=("manipulation" in mock_areas)
