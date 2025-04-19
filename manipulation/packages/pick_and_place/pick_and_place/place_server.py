aecd9e5e6eeb0a3cc9413de97b384f76f746a5d8
Emiliano Flores <114429692+EmilianoHFlores@users.noreply.github.com>
2025-04-19 15:38:36 -0400

    Manipulation full place pipeline (#280)
    * refactor: add profiles to hri
    
    * feat: download llm models on setup
    
    * docs: update hri
    
    * added moondream bag description
    
    * added tracker call
    
    * hri update
    
    * update confirmation
    
    * Ruffl formatting
    
    * Update launch vision
    
    * added point bag
    
    * Update launch
    
    * debug
    
    * callback bgr
    
    * add debug bbox
    
    * demo fixes
    
    * added qos, now using a dict
    
    * Addition of timeout to keyworkd detection command
    
    * Changed task manager
    
    * Changed task manager
    
    * Changed task manager
    
    * prints
    
    * fix
    
    * Fix crop query
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Changed task manager
    
    * Update timeout
    
    * debug
    
    * debug
    
    * Added 60 timeout and who knows whatever else
    
    * Added following carry
    
    * Changes for task manager
    
    * Added transform_target_node
    
    * Fixed_launchs_nav
    
    * added carry profile
    
    * update: hri mocks
    
    * stop stop stop stop stop stop
    
    * updated pipeline, added gripper and configurations settings
    
    * Fixed new link attached to arm in real robot launches
    
    * manip changed urdf
    
    * Test of generic task script
    
    * logger fixes
    
    * fix remove attached
    
    * test: generic task
    
    * update eigen_params
    
    * add close gripper
    
    * Nav_launch_fix and xarm_debug
    
    * Backtoback
    
    * joint positions return if close
    
    * added rtabmap launch
    
    * GPD fixes, adjusted visualizatin and configs
    
    * faster create collision objects
    
    * multiple fixes to pick pipeline
    
    * Added radial filter -> fix some calls picking up wrong plane
    
    * working full pick pipeline -> simulation, requires gripper tests
    
    * PickParams to make ManipulationTask action have a better structure
    
    * placeparams
    
    * New launch files
    
    * Kill gpd service on run, revives with launch
    
    * logger fixes and sphere size match leaf size
    
    * Update readme for new launch files
    
    * constants for zero shot
    
    * new gpd params
    
    * added params for passthrough filters -> useful to get specific planes and avoid picking up the floor or other wrong
    
    * No close point case for remove plane srv
    
    * Now heatmap returns PointStamped
    
    * Renamed PerceptionService.srv to PickPerceptionService.srv
    
    * better heatmap place algo
    
    * preparing place components
    
    * close to working pick
    
    * constants
    
    * place server ready
    
    ---------
    
    Co-authored-by: Oscar Arreola <oscar.arreola.jr@gmail.com>
    Co-authored-by: Ale-Coeto <alecoetos@gmail.com>
    Co-authored-by: david_vl2003@hotmail.com <david_vl2003@hotmail.com>
    Co-authored-by: Alejandra Coeto <a01285221@tec.mx>
    Co-authored-by: Orin <roborregosteam@gmail.com>
    Co-authored-by: GerardoFJ <A00838645@tec.mx>

--- manipulation/packages/pick_and_place/pick_and_place/place_server.py
+++ manipulation/packages/pick_and_place/pick_and_place/place_server.py
@@ -16,7 +16,7 @@ from frida_constants.manipulation_constants import (
     PLANE_NAMESPACE,
     EEF_LINK_NAME,
     EEF_CONTACT_LINKS,
-    PICK_MOTION_ACTION_SERVER,
+    PLACE_MOTION_ACTION_SERVER,
     PLANE_OBJECT_COLLISION_TOLERANCE,
 )
 from frida_interfaces.srv import (
@@ -24,26 +24,19 @@ from frida_interfaces.srv import (
     GetCollisionObjects,
     RemoveCollisionObject,
 )
-from frida_interfaces.action import PickMotion, MoveToPose
-from frida_interfaces.msg import PickResult
+from frida_interfaces.action import PlaceMotion, MoveToPose
 import copy
-import numpy as np
-from transforms3d.quaternions import quat2mat
 
 
-class PickMotionServer(Node):
+class PlaceMotionServer(Node):
     def __init__(self):
-        super().__init__("pick_server")
+        super().__init__("place_server")
         self.callback_group = ReentrantCallbackGroup()
-
-        # Declare and retrieve the parameter for the end-effector link offset
-        self.declare_parameter("ee_link_offset", -0.125)
-        self.ee_link_offset = self.get_parameter("ee_link_offset").value
 
         self._action_server = ActionServer(
             self,
-            PickMotion,
-            PICK_MOTION_ACTION_SERVER,
+            PlaceMotion,
+            PLACE_MOTION_ACTION_SERVER,
             self.execute_callback,
             callback_group=self.callback_group,
         )
@@ -71,85 +64,54 @@ class PickMotionServer(Node):
 
         self._move_to_pose_action_client.wait_for_server()
 
-        self.get_logger().info("Pick Action Server has been started")
+        self.get_logger().info("Place Action Server has been started")
 
     async def execute_callback(self, goal_handle):
-        """Execute the pick action when a goal is received."""
-        self.get_logger().info("Executing pick goal...")
+        """Execute the place action when a goal is received."""
+        self.get_logger().info("Executing place goal...")
 
         # Initialize result
-        feedback = PickMotion.Feedback()
-        result = PickMotion.Result()
+        feedback = PlaceMotion.Feedback()
+        result = PlaceMotion.Result()
         try:
-            result.success, result.pick_result = self.pick(goal_handle, feedback)
+            result.success = self.place(goal_handle, feedback)
             goal_handle.succeed()
             return result
         except Exception as e:
-            self.get_logger().error(f"Pick failed: {str(e)}")
+            self.get_logger().error(f"Place failed: {str(e)}")
             goal_handle.abort()
             result.success = False
             return result
 
-    def pick(self, goal_handle, feedback):
-        """Perform the pick operation."""
+    def place(self, goal_handle, feedback):
+        """Perform the place operation."""
         self.get_logger().info(
-            f"Trying to pick up object: {goal_handle.request.object_name}"
+            f"Trying to place up object: {goal_handle.request.object_name}"
         )
-        pick_result = PickResult()
-        grasping_poses = goal_handle.request.grasping_poses
-        for i, pose in enumerate(grasping_poses):
+        place_pose = goal_handle.request.place_pose
+
+        # generate several place_poses, so try every time higher from plane
+        n_poses = 5
+        poses_dist = 0.03
+        place_poses = []
+        for i in range(n_poses):
+            new_pose = copy.deepcopy(place_pose)
+            new_pose.pose.position.z += i * poses_dist
+            place_poses.append(new_pose)
+
+        for i, pose in enumerate(place_poses):
             # Move to pre-grasp pose
 
             ee_link_pose = copy.deepcopy(pose)
 
-            offset_distance = (
-                self.ee_link_offset
-            )  # Desired distance in meters along the local z-axis
-
-            # Compute the offset along the local z-axis
-            quat = [
-                ee_link_pose.pose.orientation.w,
-                ee_link_pose.pose.orientation.x,
-                ee_link_pose.pose.orientation.y,
-                ee_link_pose.pose.orientation.z,
-            ]
-            rotation_matrix = quat2mat(quat)
-            # Extract local Z-axis (third column of the rotation matrix)
-            z_axis = rotation_matrix[:, 2]
-
-            # Translate along the local Z-axis
-            new_position = (
-                np.array(
-                    [
-                        ee_link_pose.pose.position.x,
-                        ee_link_pose.pose.position.y,
-                        ee_link_pose.pose.position.z,
-                    ]
-                )
-                + z_axis * offset_distance
-            )
-            ee_link_pose.pose.position.x = new_position[0]
-            ee_link_pose.pose.position.y = new_position[1]
-            ee_link_pose.pose.position.z = new_position[2]
-
-            grasp_pose_handler, grasp_pose_result = self.move_to_pose(ee_link_pose)
-            print(f"Grasp Pose {i} result: {grasp_pose_result}")
-            if grasp_pose_result.result.success:
+            place_pose_handler, place_pose_result = self.move_to_pose(ee_link_pose)
+            print(f"Grasp Pose {i} result: {place_pose_result}")
+            if place_pose_result.result.success:
                 self.get_logger().info("Grasp pose reached")
-                result, lowest_obj = self.attach_pick_object()
-                if result:
-                    self.get_logger().info("Object attached")
-                    # Save object details
-                    pick_result.pick_pose = ee_link_pose
-                    pick_result.grasp_score = goal_handle.request.grasping_scores[i]
-                    pick_result.object_pick_height = self.calculate_object_pick_height(
-                        lowest_obj, ee_link_pose
-                    )
-                else:
-                    self.get_logger().error("Failed to attach object")
-                return True, pick_result
+                self.deattach_pick_object()
+                return True
         self.get_logger().error("Failed to reach any grasp pose")
-        return False, pick_result
+        return False
 
     def move_to_pose(self, pose):
         """Move the robot to the given pose."""
@@ -173,7 +135,7 @@ class PickMotionServer(Node):
         # self.get_logger().info("Execution done with status: " + str(future.result()))
         return future  # 4 is the status for success
 
-    def attach_pick_object(self):
+    def deattach_pick_object(self):
         """Attach the pick object to the robot."""
         collision_objects = self.get_collision_objects()
         # find plane object
@@ -197,7 +159,7 @@ class PickMotionServer(Node):
                         obj_lowest = obj
                 request.attached_link = EEF_LINK_NAME
                 request.touch_links = EEF_CONTACT_LINKS
-                request.detach = False
+                request.detach = True
                 self._attach_collision_object_client.wait_for_service()
                 future = self._attach_collision_object_client.call_async(request)
                 self.wait_for_future(future)
@@ -246,8 +208,8 @@ class PickMotionServer(Node):
 def main(args=None):
     rclpy.init(args=args)
     executor = rclpy.executors.MultiThreadedExecutor(5)
-    pick_server = PickMotionServer()
-    executor.add_node(pick_server)
+    place_server = PlaceMotionServer()
+    executor.add_node(place_server)
     executor.spin()
     rclpy.shutdown()
 
