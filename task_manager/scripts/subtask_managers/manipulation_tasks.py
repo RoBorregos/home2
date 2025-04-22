d4f3cab2b67920f5463052aafb73cfffa497ab17
Ivan Romero <i.wells.ar@gmail.com>
2025-04-22 08:54:12 -0600

    ??

--- task_manager/scripts/subtask_managers/manipulation_tasks.py
+++ task_manager/scripts/subtask_managers/manipulation_tasks.py
@@ -124,15 +124,20 @@ class ManipulationTasks:
         Named position has priority over joint_positions.
         """
         if named_position:
-            joint_positions = self.get_named_target(named_position)
-            # joint_positions = joint_positions["joints"]
-            # degrees = joint_positions.get("degrees", False)
+            # joint_positions = self.get_named_target(named_position)
             # joint_positions = joint_positions["positions"].keys()
+
+            joint_positions = self.get_named_target(named_position)
+
+            degrees = joint_positions.get("degrees", False)
+
+            joint_positions = joint_positions["joints"]
+
+            self.node.get_logger().info(f"dict: {joint_positions}")
 
         # Determine format of joint_positions and apply degree conversion if needed.
         if isinstance(joint_positions, dict):
-            # joint_names = list(joint_positions.keys())
-            joint_names = list(joint_positions["positions"].keys())
+            joint_names = list(joint_positions.keys())
             joint_vals = list(joint_positions.values())
             if degrees:
                 joint_vals = [x * DEG_TO_RAD for x in joint_vals]
@@ -228,8 +233,8 @@ class ManipulationTasks:
 
         try:
             future = self.follow_face_client.call_async(request)
-            rclpy.spin_until_future_complete(self.node, future, timeout_sec=10.0)
-            result = future.result()
+            rclpy.spin_until_future_complete(self.node, future, timeout_sec=TIMEOUT)
+            result = future.result().result
 
             if not result.success:
                 raise Exception("Service call failed")
