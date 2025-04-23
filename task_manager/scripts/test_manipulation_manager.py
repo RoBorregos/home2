036a578bb06e68671a9ed2425ffc600136756cec
GerardoFJ <A00838645@tec.mx>
2025-04-23 13:07:56 -0600

    test manipulation

---
+++ task_manager/scripts/test_manipulation_manager.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""
+Task Manager for testing the subtask managers
+"""
+
+import rclpy
+
+# from config.hri.debug import config as test_hri_config
+from rclpy.node import Node
+
+# from subtask_managers.hri_tasks import HRITasks
+
+from subtask_managers.manipulation_tasks import ManipulationTasks
+
+PICK_OBJECT = "zucaritas"
+
+TEST = "PICK"
+
+
+class TestTaskManager(Node):
+    def __init__(self):
+        super().__init__("test_task_manager")
+        self.subtask_manager = {}
+        # self.subtask_manager["hri"] = HRITasks(self, config=test_hri_config)
+
+        self.subtask_manager["manipulation"] = ManipulationTasks(self, task="DEMO", mock_data=False)
+
+        rclpy.spin_once(self, timeout_sec=1.0)
+        self.get_logger().info("TestTaskManager has started.")
+        self.run()
+
+    def run(self):
+        # """testing vision tasks"""
+        if TEST == "PICK":
+            self.get_logger().info(f"Trying to pick {PICK_OBJECT}")
+            result = self.subtask_manager["manipulation"].pick_object(PICK_OBJECT)
+            self.get_logger().info(f"Result: {result}")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TestTaskManager()
+
+    try:
+        rclpy.spin_once(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
