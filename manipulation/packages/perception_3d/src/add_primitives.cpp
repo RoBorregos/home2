2de818f77bca46cb35061bfe40aa4f907389570a
Iván Romero <i.wells.ar@gmail.com>
2025-04-23 16:11:08 -0600

    added details and tranforms

--- manipulation/packages/perception_3d/src/add_primitives.cpp
+++ manipulation/packages/perception_3d/src/add_primitives.cpp
@@ -1,11 +1,13 @@
 #include "frida_constants/manipulation_constants_cpp.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include <cstdint>
+
 #include <memory>
 #include <pcl/common/centroid.h>
 #include <pcl/common/common.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/impl/point_types.hpp>
+#include <queue>
 #include <rclcpp/client.hpp>
 #include <rclcpp/duration.hpp>
 #include <rclcpp/logging.hpp>
@@ -14,21 +16,26 @@
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
 
 #include <frida_constants/manipulation_constants_cpp.hpp>
-#include <perception_3d/macros.hpp>
-
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.hpp>
+#include <perception_3d/macros.hpp>
 
 #include <frida_interfaces/msg/collision_object.h>
 #include <frida_interfaces/srv/add_collision_objects.hpp>
 #include <frida_interfaces/srv/add_pick_primitives.hpp>
+#include <frida_interfaces/srv/get_plane_bbox.hpp>
+#include <frida_interfaces/srv/remove_plane.hpp>
+#include <utility>
+
+enum class PassThroughFilterType { X, Y, Z };
 
 struct BoxPrimitiveParams {
   pcl::PointXYZ centroid;
@@ -36,19 +43,27 @@ struct BoxPrimitiveParams {
   long double height;
   long double depth;
   geometry_msgs::msg::Quaternion orientation;
+  pcl::PointXYZ xy1;
+  pcl::PointXYZ xy2;
+  pcl::PointXYZ xy3;
+  pcl::PointXYZ xy4;
 };
 
 class AddPrimitivesNode : public rclcpp::Node {
 private:
   rclcpp::Service<frida_interfaces::srv::AddPickPrimitives>::SharedPtr
       add_pick_primitives_srv;
+  rclcpp::Service<frida_interfaces::srv::GetPlaneBbox>::SharedPtr
+      get_plane_bbox_srv;
   rclcpp::Client<frida_interfaces::srv::AddCollisionObjects>::SharedPtr
       add_collision_object_client;
-
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
+  rclcpp::Client<frida_interfaces::srv::RemovePlane>::SharedPtr
+      remove_plane_client;
 
   std::shared_ptr<tf2_ros::TransformListener> tf_listener;
   std::unique_ptr<tf2_ros::Buffer> tf_buffer;
+
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
 
 public:
   AddPrimitivesNode() : Node("add_primitives_node") {
@@ -57,6 +72,13 @@ public:
     this->tf_listener =
         std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
 
+    RCLCPP_INFO(this->get_logger(), "Starting Add Primitives Node");
+
+    this->tf_broadcaster =
+        std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+    RCLCPP_INFO(this->get_logger(), "Creating services");
+
     this->add_pick_primitives_srv =
         this->create_service<frida_interfaces::srv::AddPickPrimitives>(
             ADD_PICK_PRIMITIVES_SERVICE,
@@ -64,9 +86,19 @@ public:
                       std::placeholders::_1, std::placeholders::_2,
                       std::placeholders::_3));
 
+    this->get_plane_bbox_srv =
+        this->create_service<frida_interfaces::srv::GetPlaneBbox>(
+            GET_PLANE_BBOX_SERVICE,
+            std::bind(&AddPrimitivesNode::get_plane_bbox, this,
+                      std::placeholders::_1, std::placeholders::_2,
+                      std::placeholders::_3));
+
     this->add_collision_object_client =
         this->create_client<frida_interfaces::srv::AddCollisionObjects>(
             ADD_COLLISION_SERVICE);
+    this->remove_plane_client =
+        this->create_client<frida_interfaces::srv::RemovePlane>(
+            REMOVE_PLANE_SERVICE);
 
     RCLCPP_INFO(this->get_logger(), "Service created");
   }
@@ -99,36 +131,78 @@ public:
     Eigen::SelfAdjointEigenSolver<Eigen::Matrix3f> eigen_solver(
         covariance_matrix);
     Eigen::Matrix3f eigenvectors = eigen_solver.eigenvectors();
+
+    // Sort eigenvectors by eigenvalues in descending order
+    // This ensures the principal axes are properly ordered
     Eigen::Vector3f eigenvalues = eigen_solver.eigenvalues();
+    // int max_idx = 0, mid_idx = 1, min_idx = 2;
+    // if (eigenvalues(0) > eigenvalues(max_idx))
+    //   max_idx = 0;
+    // if (eigenvalues(1) > eigenvalues(max_idx))
+    //   max_idx = 1;
+    // if (eigenvalues(2) > eigenvalues(max_idx))
+    //   max_idx = 2;
 
-    eigenvectors.col(2) = (eigenvectors.col(0).cross(eigenvectors.col(1))).normalized();
-    eigenvectors.col(0) = (eigenvectors.col(1).cross(eigenvectors.col(2))).normalized();
-    eigenvectors.col(1) = (eigenvectors.col(2).cross(eigenvectors.col(0))).normalized();
+    // min_idx = (max_idx + 1) % 3;
+    // mid_idx = (max_idx + 2) % 3;
 
-    Eigen::Matrix3f eigenVectorsPCA1;
-    eigenVectorsPCA1.col(0) = eigenvectors.col(2);
-    eigenVectorsPCA1.col(1) = eigenvectors.col(1);
-    eigenVectorsPCA1.col(2) = eigenvectors.col(0);
-    eigenvectors = eigenVectorsPCA1;
+    std::priority_queue<std::pair<double, int>> pq;
+    for (int i = 0; i < 3; ++i) {
+      pq.push(std::make_pair(eigenvalues(i), i));
+    }
 
-    Eigen::Vector3f ea = (eigenvectors).eulerAngles(2, 1, 0);
-    // Eigen::AngleAxisf keep_Z_Rot(ea[0], Eigen::Vector3f::UnitZ());
+    int max_idx = pq.top().second;
+    pq.pop();
+    int mid_idx = pq.top().second;
+    pq.pop();
+    int min_idx = pq.top().second;
+    pq.pop();
+
+    // if (eigenvalues(mid_idx) < eigenvalues(min_idx))
+    //   std::swap(mid_idx, min_idx);
+
+    // Create rotation matrix with ordered eigenvectors
+    Eigen::Matrix3f rotation_matrix;
+    rotation_matrix.col(0) = eigenvectors.col(max_idx);
+    rotation_matrix.col(1) = eigenvectors.col(mid_idx);
+    rotation_matrix.col(2) = eigenvectors.col(min_idx);
+
+    // Ensure we have a right-handed coordinate system
+    if (rotation_matrix.determinant() < 0) {
+      rotation_matrix.col(2) = -rotation_matrix.col(2);
+    }
+
+    // Make sure the axes follow ROS convention (x forward, y left, z up)
+    // Align the principal components with ROS coordinate system
+    Eigen::Matrix3f ros_align = Eigen::Matrix3f::Identity();
+
+    // Convert from PCA eigenvectors to ROS convention
+    Eigen::Matrix3f aligned_rotation = rotation_matrix;
+
+    // Construct the transform
     Eigen::Affine3f transform = Eigen::Affine3f::Identity();
-
     transform.translate(center);
-    // transform.rotate(keep_Z_Rot);
-    // Convert eigenvectors (rotation matrix) to quaternion and normalize it
-    Eigen::Quaternionf quat(eigenvectors);
-    quat.normalize();
 
-    // Rotate using the quaternion directly
-    transform.rotate(quat);
+    // Create quaternion from the aligned rotation matrix
+    // For ROS2 TF compatibility, we need to ensure correct orientation
+    Eigen::Quaternionf quat(aligned_rotation);
+
+    // Using rotation from base_link frame's perspective
+    tf2::Quaternion tf_quat(quat.x(), quat.y(), quat.z(), quat.w());
+    tf2::Matrix3x3 m(tf_quat);
+
+    // Normalize and correct rotation if needed
+    tf_quat.normalize();
+
+    // Apply rotation to the transform
 
     // Set the orientation in the box parameters
-    box_params.orientation.x = quat.x();
-    box_params.orientation.y = quat.y();
-    box_params.orientation.z = quat.z();
-    box_params.orientation.w = quat.w();
+    quat.normalize();
+    box_params.orientation.x = tf_quat.x();
+    box_params.orientation.y = tf_quat.y();
+    box_params.orientation.z = tf_quat.z();
+    box_params.orientation.w = tf_quat.w();
+    transform.rotate(quat);
 
     pcl::PointCloud<pcl::PointXYZ> transformed_cloud;
     pcl::transformPointCloud(*cloud, transformed_cloud, transform);
@@ -137,6 +211,53 @@ public:
     pcl::PointXYZ min_pt, max_pt2;
     pcl::getMinMax3D(transformed_cloud, min_pt, max_pt2);
 
+    pcl::PointXYZ xy1, xy2, xy3, xy4;
+    const double z = (min_pt.z + max_pt2.z) / 2;
+    xy1.x = min_pt.x;
+    xy1.y = min_pt.y;
+    xy1.z = z;
+    xy2.x = min_pt.x;
+    xy2.y = max_pt2.y;
+    xy2.z = z;
+    xy3.x = max_pt2.x;
+    xy3.y = max_pt2.y;
+    xy3.z = z;
+    xy4.x = max_pt2.x;
+    xy4.y = min_pt.y;
+    xy4.z = z;
+    // box_params.xy1 = xy1;
+    // box_params.xy2 = xy2;
+    // box_params.xy3 = xy3;
+    // box_params.xy4 = xy4;
+
+    // how can I get min_pt and max_pt2 in the original space?
+    // Transform back to original space
+    // pcl::PointXYZ min_pt_transformed, max_pt_transformed;
+    // min_pt_transformed = pcl::transformPoint(min_pt, transform.inverse());
+    // max_pt_transformed = pcl::transformPoint(max_pt2, transform.inverse());
+
+    // // for xy points dont take into account the z axis
+    // box_params.xy1.x = min_pt_transformed.x;
+    // box_params.xy1.y = min_pt_transformed.y;
+    // box_params.xy2.x = min_pt_transformed.x;
+    // box_params.xy2.y = max_pt_transformed.y;
+    // box_params.xy3.x = max_pt_transformed.x;
+    // box_params.xy3.y = max_pt_transformed.y;
+    // box_params.xy4.x = max_pt_transformed.x;
+    // box_params.xy4.y = min_pt_transformed.y;
+    // box_params.xy1.z = z;
+    // box_params.xy2.z = z;
+    // box_params.xy3.z = z;
+    // box_params.xy4.z = z;
+    box_params.xy1 = xy1;
+    box_params.xy2 = xy2;
+    box_params.xy3 = xy3;
+    box_params.xy4 = xy4;
+    Eigen::Affine3f transform_inverse = transform.inverse();
+    box_params.xy1 = pcl::transformPoint(xy1, transform_inverse);
+    box_params.xy2 = pcl::transformPoint(xy2, transform_inverse);
+    box_params.xy3 = pcl::transformPoint(xy3, transform_inverse);
+    box_params.xy4 = pcl::transformPoint(xy4, transform_inverse);
     box_params.width = max_pt2.x - min_pt.x;
     box_params.depth = max_pt2.y - min_pt.y;
     box_params.height = std::max(max_pt2.z - min_pt.z - 0.01, 0.01);
@@ -287,6 +408,171 @@ public:
       RCLCPP_INFO(this->get_logger(), "Object primitives added");
     }
     response->status = OK;
+  }
+
+  void get_plane_bbox(
+      const std::shared_ptr<rmw_request_id_t> req_header,
+      const std::shared_ptr<frida_interfaces::srv::GetPlaneBbox::Request>
+          request,
+      const std::shared_ptr<frida_interfaces::srv::GetPlaneBbox::Response>
+          response) {
+    RCLCPP_INFO(this->get_logger(), "get_plane_bbox");
+
+    response->health_response = OK;
+
+    if (request->min_height <= 0.0 && request->max_height <= 0.0) {
+      response->health_response = INVALID_INPUT_FILTER;
+      return;
+    }
+
+    frida_interfaces::srv::RemovePlane::Request::SharedPtr req(
+        new frida_interfaces::srv::RemovePlane::Request());
+    req->min_height = request->min_height;
+    req->max_height = request->max_height;
+
+    this->remove_plane_client->async_send_request(
+        req,
+        [this, response](
+            rclcpp::Client<frida_interfaces::srv::RemovePlane>::SharedFuture
+                future) {
+          RCLCPP_INFO(this->get_logger(), "remove_plane");
+          if (future.get()->health_response != OK) {
+            response->health_response = future.get()->health_response;
+            return;
+          }
+          pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(
+              new pcl::PointCloud<pcl::PointXYZ>);
+          pcl::fromROSMsg(future.get()->cloud, *cloud_out);
+          if (cloud_out->points.size() == 0) {
+            response->health_response = NO_POINT_DETECTED;
+            return;
+          }
+          RCLCPP_INFO(this->get_logger(), "Computing box primitive");
+          RCLCPP_INFO(this->get_logger(), "Cloud size: %zu",
+                      cloud_out->points.size());
+          BoxPrimitiveParams box_params;
+          response->health_response = RansacNormals(cloud_out, box_params);
+          ASSERT_AND_RETURN_CODE(response->health_response, OK,
+                                 "Error computing box primitive with code %d",
+                                 response->health_response);
+          box_params.height = 0.025;
+          pcl::PointXYZ min_pt, max_pt2;
+          pcl::getMinMax3D(*cloud_out, min_pt, max_pt2);
+          box_params.centroid.x = min_pt.x + (max_pt2.x - min_pt.x) / 2;
+          box_params.centroid.y = min_pt.y + (max_pt2.y - min_pt.y) / 2;
+          box_params.centroid.z = min_pt.z + (max_pt2.z - min_pt.z) / 2;
+          box_params.width = max_pt2.x - min_pt.x;
+          box_params.depth = max_pt2.y - min_pt.y;
+          RCLCPP_INFO(this->get_logger(), "Box params: %Lf %Lf %Lf",
+                      box_params.width, box_params.depth, box_params.height);
+          // // box_params.height = std::max(max_pt2.z - min_pt.z - 0.01, 0.01);
+
+          response->min_x = box_params.centroid.x - box_params.width / 2;
+          response->min_y = box_params.centroid.y - box_params.depth / 2;
+          response->min_z = box_params.centroid.z - box_params.height / 2;
+          response->max_x = box_params.centroid.x + box_params.width / 2;
+          response->max_y = box_params.centroid.y + box_params.depth / 2;
+          response->max_z = box_params.centroid.z + box_params.height / 2;
+
+          // response->min_x = box_params.xy1.x;
+          // response->min_y = min_pt.y;
+          // response->min_z = min_pt.z;
+          // response->max_x = max_pt2.x;
+
+          if (true) {
+            // publish tfs 8 corners and center
+            double minn[3] = {response->min_x, response->min_y,
+                              response->min_z};
+            double maxx[3] = {response->max_x, response->max_y,
+                              response->max_z};
+
+            geometry_msgs::msg::TransformStamped transform;
+            transform.header.stamp = this->now();
+            transform.header.frame_id = "base_link";
+
+            transform.transform.translation.x = box_params.xy1.x;
+            transform.transform.translation.y = box_params.xy1.y;
+            transform.transform.translation.z = (minn[2] + maxx[2]) / 2.0;
+            // transform.transform.rotation.x = 0.0;
+            // transform.transform.rotation.y = 0.0;
+            // transform.transform.rotation.z = 0.0;
+            // transform.transform.rotation.w = 1.0;
+            transform.transform.rotation = box_params.orientation;
+            transform.child_frame_id = "plane_corner_1";
+            RCLCPP_INFO(this->get_logger(), "Sending transform");
+            this->tf_broadcaster->sendTransform(transform);
+            transform.transform.translation.x = box_params.xy2.x;
+            transform.transform.translation.y = box_params.xy2.y;
+            transform.transform.translation.z = (minn[2] + maxx[2]) / 2.0;
+            // transform.transform.rotation.x = 0.0;
+            // transform.transform.rotation.y = 0.0;
+            // transform.transform.rotation.z = 0.0;
+            // transform.transform.rotation.w = 1.0;
+            transform.transform.rotation = box_params.orientation;
+            transform.child_frame_id = "plane_corner_2";
+            this->tf_broadcaster->sendTransform(transform);
+            transform.transform.translation.x = box_params.xy3.x;
+            transform.transform.translation.y = box_params.xy3.y;
+            transform.transform.translation.z = (minn[2] + maxx[2]) / 2.0;
+            // transform.transform.rotation.x = 0.0;
+            // transform.transform.rotation.y = 0.0;
+            // transform.transform.rotation.z = 0.0;
+            // transform.transform.rotation.w = 1.0;
+            transform.transform.rotation = box_params.orientation;
+            transform.child_frame_id = "plane_corner_3";
+            this->tf_broadcaster->sendTransform(transform);
+            transform.transform.translation.x = box_params.xy4.x;
+            transform.transform.translation.y = box_params.xy4.y;
+            transform.transform.translation.z = (minn[2] + maxx[2]) / 2.0;
+            // transform.transform.rotation.x = 0.0;
+            // transform.transform.rotation.y = 0.0;
+            // transform.transform.rotation.z = 0.0;
+            // transform.transform.rotation.w = 1.0;
+            transform.transform.rotation = box_params.orientation;
+            transform.child_frame_id = "plane_corner_4";
+            this->tf_broadcaster->sendTransform(transform);
+
+            // // Publish transforms for all 8 corners
+            // for (int i = 0; i < 2; ++i) {
+            //   for (int j = 0; j < 2; ++j) {
+            //     for (int k = 0; k < 2; ++k) {
+            //       std::string corner_id = "plane_corner_" + std::to_string(i)
+            //       +
+            //                               std::to_string(j) +
+            //                               std::to_string(k);
+            //       transform.child_frame_id = corner_id;
+
+            //       double x = (i == 0) ? minn[0] : maxx[0];
+            //       double y = (j == 0) ? minn[1] : maxx[1];
+            //       double z = (k == 0) ? minn[2] : maxx[2];
+
+            //       transform.transform.translation.x = x;
+            //       transform.transform.translation.y = y;
+            //       transform.transform.translation.z = z;
+
+            //       // transform.transform.rotation.x = 0.0;
+            //       // transform.transform.rotation.y = 0.0;
+            //       // transform.transform.rotation.z = 0.0;
+            //       // transform.transform.rotation.w = 1.0;
+            //       transform.transform.rotation = box_params.orientation;
+
+            //       this->tf_broadcaster->sendTransform(transform);
+            //     }
+            //   }
+            // }
+
+            // Publish center transform
+            transform.child_frame_id = "plane_center";
+            transform.transform.translation.x = (minn[0] + maxx[0]) / 2.0;
+            transform.transform.translation.y = (minn[1] + maxx[1]) / 2.0;
+            transform.transform.translation.z = (minn[2] + maxx[2]) / 2.0;
+            transform.transform.rotation = box_params.orientation;
+            this->tf_broadcaster->sendTransform(transform);
+          }
+
+          response->health_response = OK;
+          return;
+        });
   }
 };
 
