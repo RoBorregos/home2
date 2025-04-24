d0d442d4dd605703f3d1ebbb2ee3e821438728ef
Ale-Coeto <alecoetos@gmail.com>
2025-04-24 08:30:51 -0600

    debug

--- vision/packages/vision_general/scripts/face_recognition_node.py
+++ vision/packages/vision_general/scripts/face_recognition_node.py
@@ -234,13 +234,13 @@ class FaceRecognition(Node):
 
         target = Point()
 
-        if len(self.depth_image) > 0:
-            print(xc, yc)
-            point2D = (float(xc), float(yc))
-            depth = get_depth(self.depth_image, point2D)
-            point3D = deproject_pixel_to_point(self.imageInfo, point2D, depth)
-            point3D = float(point3D[0]), float(point3D[1]), float(point3D[2])
-            target.z = point3D[2]
+        # if len(self.depth_image) > 0:
+        #     print(xc, yc)
+        #     point2D = (float(xc), float(yc))
+        #     depth = get_depth(self.depth_image, point2D)
+        #     point3D = deproject_pixel_to_point(self.imageInfo, point2D, depth)
+        #     point3D = float(point3D[0]), float(point3D[1]), float(point3D[2])
+        #     target.z = point3D[2]
 
         target.x = move_x
         target.y = move_y
