e0d68f0be5bd8b2ae7ed3fb8ce5d9ba4201f7ebd
Danaé <A00836760@tec.mx>
2025-04-24 14:03:41 -0600

    future

--- vision/packages/vision_general/scripts/gpsr_commands.py
+++ vision/packages/vision_general/scripts/gpsr_commands.py
@@ -416,7 +416,7 @@ class GPSRCommands(Node):
         if future is None:
             print("timeout reached")
             return False
-        while not future.done():
+        while not future.result():
             pass
         return future
 
