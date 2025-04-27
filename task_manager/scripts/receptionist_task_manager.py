b01f341c8513ce50bd8266a0c662e3f75b5d6e3f
Ale-Coeto <alecoetos@gmail.com>
2025-04-27 10:19:54 -0600

    display pub

--- task_manager/scripts/receptionist_task_manager.py
+++ task_manager/scripts/receptionist_task_manager.py
@@ -14,6 +14,8 @@ import time
 
 ATTEMPT_LIMIT = 3
 START = "START"
+RECEPTIONIST_VIEW = "/vision/receptionist/img_person_detecion"
+FACE_VIEW = "/vision/person_frame"
 
 
 class Guest:
@@ -109,6 +111,7 @@ class ReceptionistTM(Node):
     def set_description(self, status, description: str):
         self.get_guest().description = description
 
+
     # TODO (@alecoeto): wait to detect guest
     # TODO (@alecoeto): sleep before find seat
 
@@ -133,7 +136,7 @@ class ReceptionistTM(Node):
                 "I am ready to receive guests, please open the door.", wait=True
             )
 
-            self.subtask_manager.hri.publish_display_topic("/vision/receptionist/img_person_detecion")
+            self.subtask_manager.hri.publish_display_topic(RECEPTIONIST_VIEW)
             result = self.subtask_manager.vision.detect_person(timeout=10)
             if result == Status.EXECUTION_SUCCESS:
                 self.subtask_manager.manipulation.follow_face(True)
@@ -144,7 +147,7 @@ class ReceptionistTM(Node):
 
         if self.current_state == ReceptionistTM.TASK_STATES["GREETING"]:
             Logger.state(self, "Greeting guest")
-            self.subtask_manager.hri.publish_display_topic("/vision/person_frame")
+            self.subtask_manager.hri.publish_display_topic(FACE_VIEW)
             status, name = self.subtask_manager.hri.ask_and_confirm(
                 question="What is your name?", query="name", use_hotwords=False
             )
@@ -251,6 +254,7 @@ class ReceptionistTM(Node):
         if self.current_state == ReceptionistTM.TASK_STATES["FIND_SEAT"]:
             Logger.state(self, "Finding seat")
             # target = 0
+            self.subtask_manager.hri.publish_display_topic(RECEPTIONIST_VIEW)
             self.subtask_manager.manipulation.follow_face(False)
             self.subtask_manager.manipulation.move_joint_positions(
                 named_position="front_low_stare", velocity=0.5, degrees=True
@@ -283,6 +287,7 @@ class ReceptionistTM(Node):
 
         if self.current_state == ReceptionistTM.TASK_STATES["INTRODUCTION"]:
             Logger.state(self, "Introducing guest")
+            self.subtask_manager.hri.publish_display_topic(FACE_VIEW)
             self.subtask_manager.manipulation.move_joint_positions(
                 named_position="front_stare", velocity=0.5, degrees=True
             )
