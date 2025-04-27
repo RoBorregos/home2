dd17886b22b8a905dfc60366dbd253d40baf8738
Oscar Arreola <77394565+Oscar-gg@users.noreply.github.com>
2025-04-27 14:07:30 -0600

    Gpsr functions (#298)
    * feat(gpsr): function signatures added
    
    * fix: hri subtask
    
    * refactor: gpsr task manager
    
    * feat: added single area connector to gpsr
    
    * feat: mock command interpreter
    
    * feat: give object and ask_answer_question
    
    * feat: added example manipulation calls in gpsr
    
    * feat: added status
    
    * Added pan to and move to position manip tasks
    
    * Update tasks
    
    * fix count
    
    * return status for some functions
    
    * Added subtasks
    
    * gpsr: implemented more functions
    
    * added launch
    
    * Update launch
    
    * Fix cmake
    
    * added pck launch
    
    * fixes
    
    * testing locations
    
    * feat: get closest location with embeddings
    
    * fix ruff
    
    * temporal contextual say
    
    * fix launch
    
    * asdqweqweqwwwwwwwwww
    
    ---------
    
    Co-authored-by: Ale-Coeto <alecoetos@gmail.com>
    Co-authored-by: lleo-13 <a00830708@tec.mx>
    Co-authored-by: RoBorregos <roborregosteam@gmail.com>
    Co-authored-by: Ivan Romero <i.wells.ar@gmail.com>

---
+++ vision/packages/vision_general/launch/gpsr_launch.py
@@ -0,0 +1,38 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    detector_launch_file = os.path.join(
+        get_package_share_directory("object_detector_2d"),
+        "launch",
+        "object_detector_combined.launch.py",
+    )
+    return LaunchDescription(
+        [
+            Node(
+                package="vision_general",
+                executable="face_recognition_node.py",
+                name="face_recognition",
+                output="screen",
+                emulate_tty=True,
+                # parameters=[config],
+            ),
+            Node(
+                package="vision_general",
+                executable="gpsr_commands.py",
+                name="gpsr_commands",
+                output="screen",
+                emulate_tty=True,
+                # parameters=[config],
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(detector_launch_file)
+            ),
+        ]
+    )
