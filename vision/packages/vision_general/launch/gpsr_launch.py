103c2e6f5496cb2b0bebc645f2a4e2ce5a740513
Ale-Coeto <alecoetos@gmail.com>
2025-04-26 22:00:10 -0600

    Update launch

--- vision/packages/vision_general/launch/gpsr_launch.py
+++ vision/packages/vision_general/launch/gpsr_launch.py
@@ -4,6 +4,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
@@ -30,6 +32,10 @@ def generate_launch_description():
                 emulate_tty=True,
                 # parameters=[config],
             ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(detector_launch_file)
+            ),
         ]
+
     )
 
