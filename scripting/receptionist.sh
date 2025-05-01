b2419c16ea32bea8046700bceceed58c21d0f4a5
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-04-30 22:12:55 -0600

    feat: tmux scripting for receptionist

---
+++ scripting/receptionist.sh
@@ -0,0 +1,8 @@
+cd ./receptionist
+bash ./hri.sh
+bash ./integration.sh
+bash ./manipulation-follow-face.sh
+bash ./manipulation-motion-planning.sh
+bash ./manipulation-planning.sh
+bash ./navigation.sh
+bash ./vision.sh
