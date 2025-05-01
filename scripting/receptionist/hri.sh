032d18df6325c4385145473e6786ab8648907a52
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-05-01 07:33:42 -0600

    feat: save sudo for to authenticate scripts

--- scripting/receptionist/hri.sh
+++ scripting/receptionist/hri.sh
@@ -3,3 +3,4 @@ SESSION_NAME="hri-receptionist"
 tmux new-session -d -s $SESSION_NAME
 tmux send-keys -t $SESSION_NAME 'cd ../../' C-m
 tmux send-keys -t $SESSION_NAME 'bash run.sh hri --receptionist' C-m
+tmux send-keys -t $SESSION_NAME "$SUDO_PASSWORD" C-m
