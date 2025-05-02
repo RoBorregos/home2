2792e8d669d5345ba49ae37f41ce84f3363bc1d6
Ivan Romero <i.wells.ar@gmail.com>
2025-05-02 16:21:12 -0600

    asd

--- vision/packages/object_detector_2d/scripts/detectors/YoloV5ObjectDetector.py
+++ vision/packages/object_detector_2d/scripts/detectors/YoloV5ObjectDetector.py
@@ -19,6 +19,7 @@ class YoloV5ObjectDetector(ObjectDectector):
             "custom",
             path=self.model_path_,
             force_reload=True,
+            source="local",
         )
 
     def _inference(self, frame):
