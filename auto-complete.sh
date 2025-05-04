8c2279321ae737d8cdf1d9ed62dd867a46e63f27
AleCoeto <alecoeto@hotmail.com>
2025-05-03 19:23:03 -0600

    Autocomplete

---
+++ auto-complete.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#!/bin/bash
+
+_run_sh_autocomplete() {
+    local cur prev words cword
+    _init_completion || return
+
+    local areas="manipulation navigation hri vision integration"
+    local tasks="--carry --receptionist --storing-groceries --gpsr --moondream"
+    local flags="--rebuild --help -h -d"
+
+    case ${COMP_CWORD} in
+        1)
+            COMPREPLY=( $(compgen -W "$areas $flags" -- "$cur") )
+            ;;
+        2)
+            COMPREPLY=( $(compgen -W "$tasks $flags" -- "$cur") )
+            ;;
+        *)
+            COMPREPLY=( $(compgen -W "$flags" -- "$cur") )
+            ;;
+    esac
+}
+
+complete -F _run_sh_autocomplete ./run.sh
