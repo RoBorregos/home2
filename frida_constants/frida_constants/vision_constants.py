cfcf1c3637905ba023d66814ec9a7feee7bb155a
Danaé <A00836760@tec.mx>
2025-06-14 10:35:18 -0600

    person inside

--- frida_constants/frida_constants/vision_constants.py
+++ frida_constants/frida_constants/vision_constants.py
@@ -88,3 +88,7 @@ POSE_GESTURE_TOPIC = "/vision/gpsr/pose_gesture_detection"
 
 # Storing Groceries Commands node
 SHELF_DETECTION_TOPIC = "/vision/storing_grocPeries/shelf_detection"
+
+# Person inside a room 
+PERSON_POINT_TOPIC = "/vision/person_inside_point"
+PERSON_INSIDE_REQUEST_TOPIC = "/vision/person_inside_request"
