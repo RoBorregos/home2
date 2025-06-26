f5c4a20e40962229d009f802ec6a4137af6be014
Fernando94654 <A00841856@tec.mx>
2025-06-26 10:21:36 -0600

    reformateed files

--- vision/packages/vision_general/scripts/count_people.py
+++ vision/packages/vision_general/scripts/count_people.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import cv2
-import rclpy #python interface to ros2
+import rclpy  # python interface to ros2
 from rclpy.node import Node
 from ultralytics import YOLO
 from sensor_msgs.msg import Image
 from std_msgs.msg import Int16
-from frida_constants.vision_constants import(CAMERA_TOPIC, PEOPLE_TOPIC)
+from frida_constants.vision_constants import CAMERA_TOPIC, PEOPLE_TOPIC
 from cv_bridge import CvBridge, CvBridgeError
 from frida_interfaces.srv import CountPersons
+
+
 class CountPeople(Node):
-    def __init__(self):        
+    def __init__(self):
         super().__init__("count_people")
         self.bridge = CvBridge()
-        self.srv = self.create_service(CountPersons, 'count_persons', self.count_persons_callback)
+        self.srv = self.create_service(
+            CountPersons, "count_persons", self.count_persons_callback
+        )
         self.yolo_model = YOLO("yolov8n.pt")
         self.image = None
         self.people = 0
@@ -21,7 +25,7 @@ class CountPeople(Node):
     def count_persons_callback(self, request, response):
         print("peticion recibida")
         self.image = self.bridge.imgmsg_to_cv2(request.img, "bgr8")
-        results = self.yolo_model(self.image, verbose = False, classes = self.person_id)
+        results = self.yolo_model(self.image, verbose=False, classes=self.person_id)
         response.result = Int16()
         response.result.data = len(results[0].boxes)
         results_frame = results[0].plot()
@@ -41,5 +45,7 @@ def main(args=None):
     finally:
         count_persons_srv.destroy_node()
         rclpy.shutdown()
+
+
 if __name__ == "__main__":
     main()
--- vision/packages/vision_general/scripts/count_people_node.py
+++ vision/packages/vision_general/scripts/count_people_node.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 import cv2
-import rclpy #python interface to ros2
+import rclpy  # python interface to ros2
 from rclpy.node import Node
 from ultralytics import YOLO
 from sensor_msgs.msg import Image
 from std_msgs.msg import Int16
-from frida_constants.vision_constants import(CAMERA_TOPIC, PEOPLE_TOPIC)
+from frida_constants.vision_constants import CAMERA_TOPIC, PEOPLE_TOPIC
 from cv_bridge import CvBridge, CvBridgeError
 
+
 class CountPeople(Node):
-    def __init__(self):        
+    def __init__(self):
         super().__init__("count_people")
         self.bridge = CvBridge()
-        self.image_subscriber = self.create_subscription(Image, CAMERA_TOPIC, self.callback, 10)
+        self.image_subscriber = self.create_subscription(
+            Image, CAMERA_TOPIC, self.callback, 10
+        )
         self.detection_publisher = self.create_publisher(Int16, PEOPLE_TOPIC, 10)
         self.yolo_model = YOLO("yolov8n.pt")
         self.timer = self.create_timer(1, self.get_people)
@@ -21,9 +24,9 @@ class CountPeople(Node):
         self.person_id = 0
 
     def get_people(self):
-        #results = self.yolo_model("vision/packages/vision_general/Utils/pose_test_images/standing.jpg", verbose = False, classes = self.person_id)
+        # results = self.yolo_model("vision/packages/vision_general/Utils/pose_test_images/standing.jpg", verbose = False, classes = self.person_id)
         if self.image is not None:
-            results = self.yolo_model(self.image, verbose = False, classes = self.person_id)
+            results = self.yolo_model(self.image, verbose=False, classes=self.person_id)
             total_people = Int16()
             total_people.data = len(results[0].boxes)
             results_frame = results[0].plot()
@@ -32,7 +35,7 @@ class CountPeople(Node):
             self.detection_publisher.publish(total_people)
         else:
             print("waiting for image")
-    
+
     def callback(self, data):
         try:
             self.image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -50,5 +53,7 @@ def main(args=None):
     finally:
         node.destroy_node()
         rclpy.shutdown()
+
+
 if __name__ == "__main__":
     main()
