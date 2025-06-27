b69581c7c5bd30c505f53e690c29be66862eb944
Oscar Arreola <oscar.arreola.jr@gmail.com>
2025-06-26 18:35:21 -0600

    fix: working stt action server

--- hri/packages/speech/scripts/hear_streaming.py
+++ hri/packages/speech/scripts/hear_streaming.py
@@ -8,10 +8,12 @@ import time
 import grpc
 import numpy as np
 import rclpy
-from rclpy.executors import ExternalShutdownException
+from rclpy.action import ActionServer
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.executors import ExternalShutdownException, MultiThreadedExecutor
 from rclpy.node import Node
-from speech.speech_api_utils import SpeechApiUtils
 
+from frida_interfaces.action import SpeechStream
 from frida_interfaces.msg import AudioData
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "stt"))
@@ -20,77 +22,79 @@ import threading
 import speech_pb2
 import speech_pb2_grpc
 
-SAVE_PATH = "/workspace/src/hri/packages/speech/debug/"
-run_frames = []
-SAVE_IT = 100
-
 
 class HearStreaming(Node):
     def __init__(self):
         super().__init__("hear_streaming_node")
         self.get_logger().info("*Starting Hear Streaming Node*")
-        self.declare_parameter("publish_topic", "/rawAudioChunk")
-        self.declare_parameter("MIC_DEVICE_NAME", "default")
-        self.declare_parameter("MIC_INPUT_CHANNELS", 32)
-        self.declare_parameter("MIC_OUT_CHANNELS", 32)
-        self.declare_parameter("USE_RESPEAKER", False)
+
+        server_ip = (
+            self.declare_parameter("STT_SERVER_IP", "100.98.86.80:50051")
+            .get_parameter_value()
+            .string_value
+        )
 
         audio_topic = (
             self.declare_parameter("AUDIO_TOPIC", "/rawAudioChunk")
             .get_parameter_value()
             .string_value
         )
-        self.create_subscription(AudioData, audio_topic, self.audio_callback, 10)
 
-        publish_topic = (
-            self.get_parameter("publish_topic").get_parameter_value().string_value
-        )
-
-        self.use_respeaker = (
-            self.get_parameter("USE_RESPEAKER").get_parameter_value().bool_value
+        self.action_server_name = (
+            self.declare_parameter("STT_ACTION_SERVER_NAME", "stt_streaming")
+            .get_parameter_value()
+            .string_value
         )
 
-        mic_device_name = (
-            self.get_parameter("MIC_DEVICE_NAME").get_parameter_value().string_value
-        )
-        mic_input_channels = (
-            self.get_parameter("MIC_INPUT_CHANNELS").get_parameter_value().integer_value
-        )
-        mic_out_channels = (
-            self.get_parameter("MIC_OUT_CHANNELS").get_parameter_value().integer_value
+        default_hotwords = (
+            self.declare_parameter("DEFAULT_HOTWORDS", "Frida RoBorregos")
+            .get_parameter_value()
+            .string_value
         )
 
-        self.publisher_ = self.create_publisher(AudioData, publish_topic, 20)
-        self.input_device_index = SpeechApiUtils.getIndexByNameAndChannels(
-            mic_device_name, mic_input_channels, mic_out_channels
-        )
+        self.hotwords = default_hotwords
+        self.active_transcription = False
+        self.current_transcription = ""
+        self.stop_flag = threading.Event()
+        self.stop_flag.set()
+        self.transcript_thread = None
 
-        self.input_device_index = None
+        # gRPC Stub
+        channel = grpc.insecure_channel(server_ip)
+        self.stub = speech_pb2_grpc.SpeechStreamStub(channel)
+
         self.audio_buffer = collections.deque(maxlen=10000)
-        self.get_logger().info("Input device index: " + str(self.input_device_index))
 
-        if self.input_device_index is None:
-            self.get_logger().warn(
-                "Input device index not found, using system default."
-            )
+        subscription_group = MutuallyExclusiveCallbackGroup()
+        action_group = MutuallyExclusiveCallbackGroup()
 
-        self.get_logger().info("HearStreaming node initialized.")
+        self.create_subscription(
+            AudioData,
+            audio_topic,
+            self.audio_callback,
+            10,
+            callback_group=subscription_group,
+        )
+        self._action_server = ActionServer(
+            self,
+            SpeechStream,
+            self.action_server_name,
+            self.execute_callback,
+            callback_group=action_group,
+        )
+
+        self.get_logger().info("*Hear Streaming Node is ready*")
 
     def audio_callback(self, msg):
+        self.get_logger().info("Audio received in callback.")
         audio_data = np.frombuffer(msg.data, dtype=np.int16)
         self.audio_buffer.append(audio_data)
 
-    def record_subscribed(self):
+    def record_subscribed(self, hotwords):
         self.get_logger().info("HearStreaming node recording.")
 
-        # GRPC client setup
-        grpc_channel = grpc.insecure_channel("100.108.245.54:50051")
-        stub = speech_pb2_grpc.SpeechStreamStub(grpc_channel)
-
-        stop_flag = threading.Event()
-
         def request_generator():
-            while not stop_flag.is_set() and rclpy.ok():
+            while not self.stop_flag.is_set() and rclpy.ok():
                 try:
                     if not self.audio_buffer:
                         time.sleep(0.1)
@@ -103,45 +107,106 @@ class HearStreaming(Node):
 
                     grpc_audio = local_audio.tobytes()
                     yield speech_pb2.AudioRequest(
-                        audio_data=grpc_audio, hotwords="Roborregos"
+                        audio_data=grpc_audio, hotwords=hotwords
                     )
 
                 except IOError as e:
                     self.get_logger().error(f"I/O error({e.errno}): {e.strerror}")
                     break
+            self.stub.cancel()
 
         def handle_transcripts(responses):
-            self.get_logger().info("In handling transcripts.")
             try:
                 for response in responses:
+                    if self.stop_flag.is_set():
+                        break
                     self.get_logger().info(f"Transcript: {response.text}")
+                    self.current_transcription = response.text
             except grpc.RpcError as e:
                 self.get_logger().error(f"gRPC stream error: {e}")
 
-        responses = stub.Transcribe(request_generator())
-        response_thread = threading.Thread(target=handle_transcripts, args=(responses,))
-        response_thread.start()
+        responses = self.stub.Transcribe(request_generator())
+        self.transcript_thread = threading.Thread(
+            target=handle_transcripts, args=(responses,)
+        )
+        self.transcript_thread.start()
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info("In execute callback")
+
+        self.active_transcription = True
+        self.stop_flag.clear()
+        self.audio_buffer.clear()
+        self.current_transcription = ""
+        self.prev_transcription = ""
+
+        start_time = time.time()
+        last_word_time = start_time
+
+        if goal_handle.request.hotwords:
+            self.hotwords = goal_handle.request.hotwords
+            self.get_logger().info(f"Updated hotwords: {self.hotwords}")
+
+        self.record_subscribed(self.hotwords)
 
         try:
-            while rclpy.ok():
-                rclpy.spin_once(self, timeout_sec=0.1)
-        except KeyboardInterrupt:
-            self.get_logger().info("Stopping on user interrupt.")
-        finally:
-            stop_flag.set()
-            response_thread.join()
-            self.get_logger().info("Audio subscriber closed.")
+            while (
+                not goal_handle.is_cancel_requested
+                and time.time() - start_time < goal_handle.request.timeout
+                and time.time() - last_word_time < goal_handle.request.silence_time
+            ):
+                if self.prev_transcription != self.current_transcription:
+                    last_word_time = time.time()
+                    self.prev_transcription = self.current_transcription
+                    self.get_logger().info(
+                        f"New transcription: {self.current_transcription}"
+                    )
+                    feedback_msg = SpeechStream.Feedback()
+                    feedback_msg.current_transcription = self.prev_transcription
+                    goal_handle.publish_feedback(feedback_msg)
+                # rclpy.spin_once(self, timeout_sec=0.1)
+                time.sleep(0.1)
+        except Exception as e:
+            self.get_logger().error(f"Execution interrupted: {e}")
+
+        if time.time() - last_word_time >= goal_handle.request.silence_time:
+            self.get_logger().info(
+                f"Silence detected for more than {goal_handle.request.silence_time}, stopping transcription."
+            )
+        elif time.time() - start_time >= goal_handle.request.timeout:
+            self.get_logger().info(
+                f"Timeout of {goal_handle.request.timeout} seconds reached, stopping transcription."
+            )
+        elif goal_handle.is_cancel_requested:
+            self.get_logger().info("Action goal cancelled, stopping transcription.")
+        else:
+            self.get_logger().info("Transcription completed successfully.")
+
+        self.stop_flag.set()
+        self.transcript_thread.join()
+        goal_handle.succeed()
+        result = SpeechStream.Result()
+        result.transcription = self.current_transcription.strip()
+        self.get_logger().info(f"Final transcription: {result.transcription}")
+        return result
 
 
 def main(args=None):
     rclpy.init(args=args)
+    node = HearStreaming()
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+
     try:
-        n = HearStreaming()
-        n.record_subscribed()
+        executor.spin()
     except (ExternalShutdownException, KeyboardInterrupt):
         pass
     finally:
-        rclpy.try_shutdown()
+        node.stop_flag.set()
+        if node.transcript_thread and node.transcript_thread.is_alive():
+            node.transcript_thread.join()
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
