c4055b20dc9dc40f536dbd6bce4b120c76be2a2f
Oscar Arreola <77394565+Oscar-gg@users.noreply.github.com>
2025-07-04 16:17:58 -0600

    end of an era 🪦 (#468)

--- hri/packages/embeddings/scripts/filter.py
+++
@@ -1,6 +0,0 @@
-def remove_nulls(obj: dict) -> dict:
-    return {k: v for k, v in obj.items() if v is not None}
-
-
-def remove_empty_lists(obj: dict) -> dict:
-    return {k: v for k, v in obj.items() if v != []}
