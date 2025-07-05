5a895e4f5b9c9c88ea109ca0bb3eabca3f2a12e3
AleGonzcamilla <A00839233@tec.mx>
2025-07-04 20:57:56 -0600

    pre-commit fixtures

--- manipulation/packages/xarm_utils/xarm_utils/ir_gripper.py
+++ manipulation/packages/xarm_utils/xarm_utils/ir_gripper.py
@@ -3,20 +3,21 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Bool
 from xarm_msgs.srv import GetDigitalIO
 from frida_interfaces.msg import GripperDetection
 
+
 class IRGripper(Node):
-
     def __init__(self):
-        super().__init__('ir_gripper_node')
+        super().__init__("ir_gripper_node")
 
-        self.cli = self.create_client(GetDigitalIO, '/xarm/get_tgpio_digital')
-        #while not self.cli.wait_for_service(timeout_sec=1.0):
-        #    self.get_logger().info('Waiting for get_tgpio_digital service...')
+        self.cli = self.create_client(GetDigitalIO, "/xarm/get_tgpio_digital")
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Waiting for get_tgpio_digital service...")
 
-        self.tool_gpio_1_pub = self.create_publisher(GripperDetection, 'ir_detection', 10)
+        self.tool_gpio_1_pub = self.create_publisher(
+            GripperDetection, "ir_detection", 10
+        )
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
@@ -30,11 +31,12 @@ class IRGripper(Node):
                 msg = GripperDetection()
                 msg.ir_detection = bool(response.digitals[1])
                 self.tool_gpio_1_pub.publish(msg)
-                self.get_logger().info(f'Published GPIO states: {msg.ir_detection}')
+                self.get_logger().info(f"Published GPIO states: {msg.ir_detection}")
             except Exception as e:
-                self.get_logger().error(f'Service call failed: {str(e)}')
+                self.get_logger().error(f"Service call failed: {str(e)}")
 
         future.add_done_callback(callback)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -45,5 +47,6 @@ def main(args=None):
     ir_gripper.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
