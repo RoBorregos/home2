ec98069376518a10b7e609239834921ef409a74f
Orin <roborregosteam@gmail.com>
2025-07-07 10:49:18 -0600

    update custom llm download

--- hri/packages/nlp/assets/download-model.sh
+++ hri/packages/nlp/assets/download-model.sh
@@ -2,12 +2,11 @@
 
 # Download model and Modelfile to the directory where this script is located
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-[ ! -f "$SCRIPT_DIR/NewModelfile" ] && curl -L https://huggingface.co/diegohc/rbrgs-finetuning/resolve/new-format/f16/Modelfile -o "$SCRIPT_DIR/NewModelfile"
-[ ! -f "$SCRIPT_DIR/new-rbrgs-finetuned.F16.gguf" ] && curl -L https://huggingface.co/diegohc/rbrgs-finetuning/resolve/new-format/f16/unsloth.F16.gguf -o "$SCRIPT_DIR/new-rbrgs-finetuned.F16.gguf"
+[ ! -f "$SCRIPT_DIR/rbrgs.F16.gguf" ] && curl -L https://huggingface.co/diegohc/rbrgs-finetuning/resolve/paraphrased-dataset/q4/unsloth.Q4_K_M.gguf -o "$SCRIPT_DIR/rbrgs.F16.gguf"
 
 # Detect available image
 if docker images | grep -q "dustynv/ollama"; then
-    IMAGE="dustynv/ollama:r36.4.0"
+    IMAGE="dustynv/ollama:0.6.8-r36.4"
     COMMAND="ollama serve"
 elif docker images | grep -q "ollama/ollama"; then
     IMAGE="ollama/ollama"
@@ -23,8 +22,9 @@ echo "Running: docker run -d --rm --runtime=nvidia -v \"$SCRIPT_DIR\":/ollama -e
 
 CONTAINER_ID=$(docker run -d --rm --runtime=nvidia -v $SCRIPT_DIR:/ollama -e OLLAMA_MODELS=/ollama "$IMAGE" $COMMAND)
 
-docker exec "$CONTAINER_ID" ollama create -f /ollama/NewModelfile rbrgs-finetuning
+docker exec "$CONTAINER_ID" ollama create -f /ollama/Modelfile rbrgs
 docker exec "$CONTAINER_ID" ollama pull nomic-embed-text
+docker exec "$CONTAINER_ID" ollama pull qwen3
 docker exec "$CONTAINER_ID" ollama pull qwen2.5
 
 docker stop "$CONTAINER_ID"
