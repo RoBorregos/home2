f0ff276aa62df1e3c202fa54be88d959fa8b6070
Emiliano Flores <114429692+EmilianoHFlores@users.noreply.github.com>
2025-07-09 19:25:39 -0400

    Manipulation - Place in given pose (#530)
    * update dependencies
    
    * sim update
    
    * New place task usage with forced pose to place in
    
    * added force pose test in keyboard_input

--- manipulation/packages/pick_and_place/pick_and_place/managers/PlaceManager.py
+++ manipulation/packages/pick_and_place/pick_and_place/managers/PlaceManager.py
@@ -1,4 +1,5 @@
 from frida_motion_planning.utils.ros_utils import wait_for_future
+from frida_motion_planning.utils.tf_utils import transform_pose
 from frida_interfaces.srv import PlacePerceptionService, HeatmapPlace
 from geometry_msgs.msg import PoseStamped
 from frida_interfaces.msg import PlaceParams
@@ -75,37 +76,65 @@ class PlaceManager:
     def get_place_pose(
         self, place_params: PlaceParams, pick_result: PickResult
     ) -> PoseStamped:
-        # Call Perception Service to get object cluster and generate collision objects
-        self.node.get_logger().info("Extracting table cloud")
-        request = PlacePerceptionService.Request()
-        request.place_params = place_params
-        self.node.place_perception_3d_client.wait_for_service()
-        future = self.node.place_perception_3d_client.call_async(request)
-        wait_for_future(future)
-        pcl_result = future.result().cluster_result
-        if len(pcl_result.data) == 0:
-            self.node.get_logger().error("No plane cluster detected")
-            return None
-        self.node.get_logger().info(
-            f"Plane cluster detected: {len(pcl_result.data)} points"
-        )
-
-        heatmap_request = HeatmapPlace.Request()
-        heatmap_request.pointcloud = pcl_result
-        if place_params.is_shelf:
-            heatmap_request.prefer_closest = True
-
-        self.node.place_pose_client.wait_for_service()
-        future = self.node.place_pose_client.call_async(heatmap_request)
-        wait_for_future(future)
-        point_result = future.result().place_point
-
-        self.node.get_logger().info(
-            f"Place point detected: {point_result.point.x}, {point_result.point.y}, {point_result.point.z}"
-        )
         result_pose = PoseStamped()
-        result_pose.header = point_result.header
-        result_pose.pose.position = point_result.point
+
+        if place_params.forced_pose.header.frame_id != "":
+            self.node.get_logger().info("Using forced place pose")
+            result_pose = place_params.forced_pose
+            # transform forced pose to the correct frame, mainly so we can alter orientation correctly
+            # by default we go to the base_link frame
+            # if we have pick_result, we use that frame -> most likely base_link too but we aim to be flexible
+
+            if pick_result.pick_pose.header.frame_id != "":
+                transform_frame = pick_result.pick_pose.header.frame_id
+            else:
+                transform_frame = "base_link"
+            self.node.get_logger().info(
+                f"Transforming forced place pose to {transform_frame} frame"
+            )
+            success, result_pose = transform_pose(
+                result_pose, transform_frame, self.node.tf_buffer
+            )
+            if not success:
+                self.node.get_logger().error("Failed to transform forced place pose")
+                return None
+            self.node.get_logger().info(
+                f"Transformed forced place pose: {result_pose.pose.position.x}, "
+                f"{result_pose.pose.position.y}, {result_pose.pose.position.z}"
+            )
+        else:
+            # Call Perception Service to get object cluster and generate collision objects
+            self.node.get_logger().info("Extracting table cloud")
+            request = PlacePerceptionService.Request()
+            request.place_params = place_params
+            self.node.place_perception_3d_client.wait_for_service()
+            future = self.node.place_perception_3d_client.call_async(request)
+            wait_for_future(future)
+            pcl_result = future.result().cluster_result
+            if len(pcl_result.data) == 0:
+                self.node.get_logger().error("No plane cluster detected")
+                return None
+
+            self.node.get_logger().info(
+                f"Plane cluster detected: {len(pcl_result.data)} points"
+            )
+
+            heatmap_request = HeatmapPlace.Request()
+            heatmap_request.pointcloud = pcl_result
+
+            if place_params.is_shelf:
+                heatmap_request.prefer_closest = True
+            self.node.place_pose_client.wait_for_service()
+            future = self.node.place_pose_client.call_async(heatmap_request)
+            wait_for_future(future)
+            point_result = future.result().place_point
+
+            self.node.get_logger().info(
+                f"Place point detected: {point_result.point.x}, {point_result.point.y}, {point_result.point.z}"
+            )
+
+            result_pose.header = point_result.header
+            result_pose.pose.position = point_result.point
 
         if (
             pick_result.object_pick_height == 0
