5ad37d91026e310643f106b29af6962724f3b579
Didi-121 <edidier11@outlook.com>
2026-02-17 18:20:18 -0600

    Improve db population (#567) (#631)
    * Improve db population (#567)
    - Added a flag to run.sh that runs regenerate_db.sh
    - This script deletes current dumps, generate new dumps based on the jsons, truncate tables at DB and insert the new dumps
    - Auto complete modified for this flag
    - ELiminated redundancies on create_Sql_dump.py
    - Conserved dummy data for each table (data used at robocup 2025)
    
    * fix/correct regenerate_db flag handling
    
    -Changed the behavior of the argument from a task to an optional setup process
    -The tables are now detected dynamically and only the tables with an existent sql dump are regenerated
    -Minor changes as: deletion of commented code, unesessary checks, defaulting, etc
    
    * fix: context_areas.json restored

--- docker/hri/run.sh
+++ docker/hri/run.sh
@@ -14,6 +14,7 @@ BUILD_IMAGE=""
 BUILD_DISPLAY=""
 OPEN_DISPLAY=""
 DOWNLOAD_MODEL=""
+REGENERATE_DB=""
 
 COMPOSE="compose/docker-compose-${ENV_TYPE}.yml"
 
@@ -49,6 +50,8 @@ for arg in "${ARGS[@]}"; do
     "--download-model")
         DOWNLOAD_MODEL="true"
         ;;
+    "--regenerate-db")
+        REGENERATE_DB="true"
   esac
 done
 
@@ -103,6 +106,12 @@ if [ ! -d "$DISPLAY_DIR/node_modules" ] || [ ! -d "$DISPLAY_DIR/.next" ] || [ "$
   docker compose -f compose/hri-ros.yaml run $BUILD_IMAGE --rm --entrypoint "" hri-ros bash -c "cd /workspace/src/hri/packages/display/display && npm i && npm run build"
 fi
 
+# Regenerate database if requested
+if [ "$REGENERATE_DB" == "true" ]; then
+  echo "Regenerating database..."
+  bash scripts/regenerate_db.sh "$ENV_TYPE"
+fi
+exit 0 
 #_________________________RUN_________________________
 
 GENERATE_BAML_CLIENT="baml-cli generate --from /workspace/src/task_manager/scripts/utils/baml_src/"
