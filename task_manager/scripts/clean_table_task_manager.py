53eaec2f433ebaf3acc49743c2903ceb6f00d99c
Gilberto Malagamba Montejo <148925854+GilMM27@users.noreply.github.com>
2026-02-26 09:55:25 -0600

    updated ask_and_confirm (#669)
    * updated ask_and_confirm
    
    * inverted logic operator
    
    * bundle find_closest result
    
    * move timout to constants
    
    * and/or

--- task_manager/scripts/clean_table_task_manager.py
+++ task_manager/scripts/clean_table_task_manager.py
@@ -213,7 +213,7 @@ class CleanTableTM(Node):
             #     labels,
             #     object_to_pick,
             # )
-            # self.detected_object = target[0]
+            # self.detected_object = target.results[0]
 
             self.subtask_manager.hri.say(
                 f"I have detected a {self.detected_object} on the table. I will now try to pick it up.",
