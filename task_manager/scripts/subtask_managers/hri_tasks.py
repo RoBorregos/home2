27ce8086e62f5c83461f647084063ceba77ae142
Luis Alvaro Rosales Salazar <luisrosalessalazar852@gmail.com>
2026-03-08 14:17:57 -0600

    wired up initial prompt

--- task_manager/scripts/subtask_managers/hri_tasks.py
+++ task_manager/scripts/subtask_managers/hri_tasks.py
@@ -420,6 +420,7 @@ class HRITasks(metaclass=SubtaskMeta):
     def hear(
         self,
         hotwords="",
+        initial_prompt="",
         silence_time=2.0,
         start_silence_time=4.0,
         max_audio_length=13.0,
@@ -431,6 +432,7 @@ class HRITasks(metaclass=SubtaskMeta):
 
         accepted_future = self.hear_streaming(
             hotwords=hotwords,
+            initial_prompt=initial_prompt,
             silence_time=silence_time,
             start_silence_time=start_silence_time,
             timeout=max_audio_length,
@@ -477,6 +479,7 @@ class HRITasks(metaclass=SubtaskMeta):
         self,
         timeout: float = 13.0,
         hotwords: str = "",
+        initial_prompt: str = "",
         silence_time: float = 2.0,
         start_silence_time: float = 4.0,
     ):
@@ -499,6 +502,7 @@ class HRITasks(metaclass=SubtaskMeta):
         goal_msg = SpeechStream.Goal()
         goal_msg.timeout = float(timeout)
         goal_msg.hotwords = hotwords
+        goal_msg.initial_prompt = initial_prompt
         self.last_hotwords = hotwords
         goal_msg.silence_time = float(silence_time)
         goal_msg.start_silence_time = float(start_silence_time)
