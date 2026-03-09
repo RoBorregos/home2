fe2f8e046246a9df7f764e4da4205025f1112ce2
GilMM27 <A00839075@tec.mx>
2026-03-08 18:06:44 -0600

    change constant name to hric

--- frida_constants/frida_constants/vision_constants.py
+++ frida_constants/frida_constants/vision_constants.py
@@ -106,7 +106,7 @@ FOLLOW_BY_TOPIC = "/vision/follow_by_name"
 CHECK_PERSON_TOPIC = "/vision/hric/detect_person"
 FIND_SEAT_TOPIC = "/vision/hric/find_seat"
 IMAGE_TOPIC_HRIC = "/vision/hric/img_person_detecion"
-DETECT_HAND_SERVICE = "/vision/receptionist/detect_hand"
+DETECT_HAND_SERVICE = "/vision/hric/detect_hand"
 
 # GPSR commands node
 COUNT_BY_COLOR_TOPIC = "/vision/gpsr/count_by_color"
