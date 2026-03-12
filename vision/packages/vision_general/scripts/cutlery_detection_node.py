b5fb12485dff3a12b6c42b66b304f803c5402880
Dany Herrera <A00838702@tec.mx>
2026-03-11 20:58:26 -0600

    refactor: change to custom model for cutlery detection

--- vision/packages/vision_general/scripts/cutlery_detection_node.py
+++ vision/packages/vision_general/scripts/cutlery_detection_node.py
@@ -3,14 +3,22 @@
 Node to detect cutlery objects (knife, spoons and forks).
 """
 
+import pathlib
 import rclpy
+import cv2
+from sensor_msgs.msg import Image
+from frida_interfaces.msg import Detection
+from frida_interfaces.srv import YoloDetect
 from rclpy.node import Node
 from cv_bridge import CvBridge
-from frida_interfaces.srv import YoloDetect
+from ultralytics import YOLO
+from frida_constants.vision_constants import CAMERA_TOPIC
+from ament_index_python.packages import get_package_share_directory
 
 FORK_CLASS = 42
 KNIFE_CLASS = 43
 SPOON_CLASS = 44
+CONF_THRESHOLD = 0.2
 
 
 class CutleryDetectionNode(Node):
@@ -20,32 +28,117 @@ class CutleryDetectionNode(Node):
         self.bridge = CvBridge()
         self.image = None
 
-        self.yolo_client = self.create_client(YoloDetect, "yolo_detect")
+        MODELS_PATH = (
+            pathlib.Path(get_package_share_directory("vision_general"))
+            / "Utils"
+            / "models"
+        )
 
-        while not self.yolo_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info("Waiting for yolo_node service to be availble...")
+        self.cutlery_model = YOLO(str(MODELS_PATH / "cutlery.pt"))
+        self.get_logger().info("Cutlery model loaded")
 
-        self.timer = self.create_timer(2.0, self.detect_cutlery)
+        self.image_subscriber = self.create_subscription(
+            Image, CAMERA_TOPIC, self.image_callback, 10
+        )
 
-    def detect_cutlery(self):
-        request = YoloDetect.Request()
-        request.classes = [FORK_CLASS, KNIFE_CLASS, SPOON_CLASS]
+        self.cutlery_detections_publisher = self.create_publisher(
+            Image, "cutlery_detections", 5
+        )
 
-        future = self.yolo_client.call_async(request)
-        future.add_done_callback(self.detection_callback)
+        self.detect_service = self.create_service(
+            YoloDetect, "cutlery_detector", self.detect_callback
+        )
 
-    def detection_callback(self, future):
-        try:
-            response = future.result()
-            if response.success:
-                for det in response.detections:
-                    self.get_logger().info(
-                        f"bbox=({det.x1}, {det.y1}, {det.x2}, {det.y2})"
-                    )
-            else:
-                self.get_logger().warn("No image available")
-        except Exception as e:
-            self.get_logger().error(f"Service call failed: {e}")
+    def image_callback(self, data):
+        self.image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        self.publish_cutlery_annotations()
+
+    def detect_callback(self, req, res):
+        """Run YOLO on the latest cached frame."""
+        if self.image is None:
+            self.get_logger().warn("No image received yet from camera.")
+            res.success = False
+            res.detections = "[]"
+            self.get_logger().info(
+                f"YOLO res: success={res.success}, detections={res.detections}"
+            )
+            return res
+
+        results = self.cutlery_model(self.image, verbose=False, classes=[0, 1, 3])
+
+        detections = []
+        annotated = self.image.copy()
+        for out in results:
+            for box in out.boxes:
+                conf = box.conf.item()
+                if conf < CONF_THRESHOLD:
+                    continue
+                x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
+                cls_id = int(box.cls.item())
+
+                det = Detection()
+                det.x1 = x1
+                det.y1 = y1
+                det.x2 = x2
+                det.y2 = y2
+                det.confidence = conf
+                det.class_id = cls_id
+
+                detections.append(det)
+                label = f"{self.cutlery_model.names[cls_id]}: {conf:.2f}"
+                color = (0, 255, 0)
+                cv2.rectangle(annotated, (x1, y1), (x2, y2), color, 2)
+                cv2.putText(
+                    annotated,
+                    label,
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.6,
+                    color,
+                    2,
+                )
+
+        # Publish annotated image to topic
+        annotated_msg = self.bridge.cv2_to_imgmsg(annotated, encoding="bgr8")
+        self.cutlery_detections_publisher.publish(annotated_msg)
+
+        res.success = True
+        res.detections = detections
+        self.get_logger().info(
+            f"YOLO res: success={res.success}, detections={[(d.class_id, d.confidence) for d in detections]}"
+        )
+        return res
+
+    def publish_cutlery_annotations(self):
+        """Detect cutlery and publish annotated image"""
+        if self.image is None:
+            return
+
+        results = self.cutlery_model(self.image, verbose=False, classes=[0, 1, 3])
+        annotated = self.image.copy()
+        for res in results:
+            for box in res.boxes:
+                conf = box.conf.item()
+                if conf < CONF_THRESHOLD:
+                    continue
+                x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
+                cls_id = int(box.cls.item())
+                label = f"{self.cutlery_model.names[cls_id]}: {conf:.2f}"
+                self.get_logger().info(f"{label} at ({x1}, {y1}), ({x2}, {y2})")
+                color = (0, 255, 0)
+                cv2.rectangle(annotated, (x1, y1), (x2, y2), color, 2)
+                cv2.putText(
+                    annotated,
+                    label,
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.6,
+                    color,
+                    2,
+                )
+
+        annotated_msg = self.bridge.cv2_to_imgmsg(annotated, encoding="bgr8")
+        self.cutlery_detections_publisher.publish(annotated_msg)
 
 
 def main(args=None):
