456042aaeb15e10f255f94802471ee1fe92d7514
GerardoFJ <A00838645@tec.mx>
2026-03-26 00:48:01 -0600

    Roudi Fix

--- docker/vision/run.sh
+++ docker/vision/run.sh
@@ -89,7 +89,7 @@ mkdir -p install build log moondream/install moondream/build moondream/log
 
 #_________________________RUN_________________________
 
-SOURCE_ROS="source /opt/ros/humble/setup.bash"
+SOURCE_ROS="source /opt/ros/humble/setup.bash && source /usr/local/bin/cyclonedds_setup.sh"
 SOURCE_INTERFACES="if [ -f frida_interfaces_cache/install/local_setup.bash ]; then source frida_interfaces_cache/install/local_setup.bash; fi"
 IGNORE_PACKAGES="--packages-ignore frida_interfaces frida_constants"
 MOONDREAM_PACKAGES="moondream_run"
