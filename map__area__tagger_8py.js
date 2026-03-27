d3ce97c38534218df104d57cffb7a0b58ce04a7d
CamilaTite26 <a00841255@tec.mx>
2026-03-27 08:17:04 +0000

    Deploying to gh-pages from @ RoBorregos/home2@8b731519c81b88ae2de690f192a223fd3dba7d71 🚀

--- map__area__tagger_8py.js
+++ map__area__tagger_8py.js
@@ -5,5 +5,6 @@ var map__area__tagger_8py =
     [ "main", "map__area__tagger_8py.html#a33c0e02922d99d9405848a521fad78ab", null ],
     [ "AREA_COLORS", "map__area__tagger_8py.html#a0a8c16661fedc07a4d89252f175f3fde", null ],
     [ "LOCATION_COLOR", "map__area__tagger_8py.html#aa51966e06f7d42b83f9394fc912a38ab", null ],
-    [ "POLYGON_VERTEX_COLOR", "map__area__tagger_8py.html#a5ff188d3d9297ca930d16ba3349e0f0d", null ]
+    [ "POLYGON_VERTEX_COLOR", "map__area__tagger_8py.html#a5ff188d3d9297ca930d16ba3349e0f0d", null ],
+    [ "ROBOCUP_TASKS", "map__area__tagger_8py.html#a8b8e125ab7b5ed1f090345e6c568893f", null ]
 ];
--- namespacemap__area__tagger.js
+++ namespacemap__area__tagger.js
@@ -5,5 +5,6 @@ var namespacemap__area__tagger =
     [ "main", "namespacemap__area__tagger.html#a33c0e02922d99d9405848a521fad78ab", null ],
     [ "AREA_COLORS", "namespacemap__area__tagger.html#a0a8c16661fedc07a4d89252f175f3fde", null ],
     [ "LOCATION_COLOR", "namespacemap__area__tagger.html#aa51966e06f7d42b83f9394fc912a38ab", null ],
-    [ "POLYGON_VERTEX_COLOR", "namespacemap__area__tagger.html#a5ff188d3d9297ca930d16ba3349e0f0d", null ]
+    [ "POLYGON_VERTEX_COLOR", "namespacemap__area__tagger.html#a5ff188d3d9297ca930d16ba3349e0f0d", null ],
+    [ "ROBOCUP_TASKS", "namespacemap__area__tagger.html#a8b8e125ab7b5ed1f090345e6c568893f", null ]
 ];
