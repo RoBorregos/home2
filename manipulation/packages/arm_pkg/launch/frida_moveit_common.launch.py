b5a6562dad01e6f4eef50b8f97fe29a66521f9be
JLDominguezM <A01285873@tec.mx>
2026-03-27 21:28:03 -0600

    Update octomap frame reference and enable octomap configuration in launch setup

--- manipulation/packages/arm_pkg/launch/frida_moveit_common.launch.py
+++ manipulation/packages/arm_pkg/launch/frida_moveit_common.launch.py
@@ -73,7 +73,7 @@ def launch_setup(context, *args, **kwargs):
     moveit_config_package_name = "xarm_moveit_config"
 
     octomap_config = {
-        "octomap_frame": "base_link",
+        "octomap_frame": "link_base",
         "octomap_resolution": 0.025,
         "max_range": 2.0,
     }
@@ -92,8 +92,8 @@ def launch_setup(context, *args, **kwargs):
             {"vamp.response_adapters": "default_planner_response_adapters/AddTimeOptimalParameterization"},
             {"planning_scene_monitor_options": {"publish_planning_scene": True}},
             {"check_solution_paths": False},
-#            octomap_config,
-#            octomap_updater_config,
+            octomap_config,
+            octomap_updater_config,
         ],
     )
 
