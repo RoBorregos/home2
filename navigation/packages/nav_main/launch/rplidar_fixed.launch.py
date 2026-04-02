84f55b2a24f5ea8ca54aed7f71d70fadc3bef5e2
CamilaTite26 <a00841255@tec.mx>
2026-04-02 10:53:39 -0600

    refactor: remove laser ignore node, update lidar configuration, add map data, and initialize solution file

--- navigation/packages/nav_main/launch/rplidar_fixed.launch.py
+++ navigation/packages/nav_main/launch/rplidar_fixed.launch.py
@@ -15,18 +15,6 @@ def generate_launch_description():
     return LaunchDescription([
         declare_use_sim,
         Node(
-            package='nav_main',
-            executable='ignore_laser',
-            parameters = [{'ignore_array': '-139.9, -122.4, -86.4, -78.4, -40.8, -30.3, 0, 21.8, 30.8, 40.3, 78.4, 93.9, 120.9, 138.4'}],
-	    #parameters = [{'ignore_array': '-138.4, -120.4, -86.9, -78.4, -39.8, -32.3, -22.3, -4.8, 33.3, 39.8, 77.9, 86.9, 122.4, 139.9'}],
-            condition=UnlessCondition(use_sim)
-        ),
-        Node(
-            package='nav_main',
-            executable='ignore_laser',
-            condition=IfCondition(use_sim)
-        ),
-        Node(
             package='sllidar_ros2',
             executable='sllidar_node',
             name='sllidar_node',
@@ -36,10 +24,10 @@ def generate_launch_description():
                          'frame_id': 'laser',
                          'inverted': True,
                          'angle_compensate': True,
-                         'scan_mode': 'Standard'}],
+                         'scan_mode': 'Standard',
+                         'ignore_array': '-139.9, -122.4, -86.4, -78.4, -40.8, -30.3, 0, 21.8, 30.8, 40.3, 78.4, 93.9, 120.9, 138.4',
+                         'min_range': 0.12}],
             output='screen',
-            remappings=[
-            ('/scan', '/scan_input')],
             condition=UnlessCondition(use_sim),
-            )
+            ),
     ])
