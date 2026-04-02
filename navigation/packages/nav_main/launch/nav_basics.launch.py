e7e88c7ecb8491a97a0e9f7f89a05c9d8bd07f89
CamilaTite26 <a00841255@tec.mx>
2026-04-02 12:07:48 -0600

    feat: update lidar launch configuration

--- navigation/packages/nav_main/launch/nav_basics.launch.py
+++ navigation/packages/nav_main/launch/nav_basics.launch.py
@@ -57,22 +57,22 @@ def launch_setup(context, *args, **kwargs):
         }.items(),
     )
 
-    laser_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("sllidar_ros2"),
-                    "launch",
-                    "sllidar_c1_launch.py",
-                ]
-            )
-        ),
-        launch_arguments={
+    laser_launch = Node(
+        package='sllidar_ros2',
+        executable='sllidar_node',
+        name='sllidar_node',
+        parameters=[{
+            'channel_type': 'serial',
             'serial_port': '/dev/ttyUSBlidar2',
-            'inverted': 'true',
+            'serial_baudrate': 460800,
+            'frame_id': 'laser',
+            'inverted': True,
+            'angle_compensate': True,
+            'scan_mode': 'Standard',
             'ignore_array': '-139.9, -122.4, -86.4, -78.4, -40.8, -30.3, 0, 21.8, 30.8, 40.3, 78.4, 93.9, 120.9, 138.4',
-            'min_range': '0.12',
-        }.items(),
+            'min_range': 0.12,
+        }],
+        output='screen',
         condition=UnlessCondition(use_sim)
         )
     
