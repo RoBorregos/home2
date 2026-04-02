fd5ebf74fd076c05977523fa086cbc9cd6d17fc7
Gerardo Fregoso Jiménez <143139229+GerardoFJ@users.noreply.github.com>
2026-04-02 11:54:50 -0600

    Check door updated (#839)
    * Added lidar retreive to lifecycle
    
    * Updated type on function
    
    * Updated issues
    
    * Testing average
    
    * Fixed and typo
    
    * Test executor
    
    * Test multithreading
    
    * Fixed service stuck
    
    * Updated import
    
    * Changed range
    
    * Updated logic to ranges
    
    * Fixed filter
    
    * Test return service
    
    * Updated variable door
    
    * Updated point recongnition
    
    * Updated logs and lidar prevention
    
    * Fix duration issue
    
    * Fixed typo
    
    * constant arquitecture
    
    * Fixed task manager package includes
    
    * Updated precommit

--- frida_constants/frida_constants/navigation_constants.py
+++ frida_constants/frida_constants/navigation_constants.py
@@ -1,3 +1,29 @@
-GOAL_TOPIC = "/navigate_to_pose"
-FOLLOWING_SERVICE = "/navigation/activate_following"
+from enum import Enum
+
+# Navigation Manager
+# -----------------------------------------------------------------
+###Check door service
+CHECK_DOOR_SERVICE = "/is_door_open"
+
+
+class DOOR_CHECK(Enum):
+    TIMEOUT_SENSOR = 5.0
+    TIMEOUT_TO_OPEN = 60.0  # Increase in case of required
+    LIDAR_RANGE_MIN = 670
+    LIDAR_RANGE_MAX = 70
+    CHECKING_RATE = 0.5
+    DOOR_DISTANCE = 1.0
+
+
+###Map areas service
 AREAS_SERVICE = "/areas_json"
+
+# ------------------------------------------------------------------
+
+# General constants
+GOAL_TOPIC = "/navigate_to_pose"
+SCAN_TOPIC = "/scan"
+
+
+# To be changed
+FOLLOWING_SERVICE = "/navigation/activate_following"
