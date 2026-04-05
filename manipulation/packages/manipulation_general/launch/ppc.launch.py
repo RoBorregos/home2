942c4d923636b9efe52f75ab7b005f129e13a16f
JLDominguezM <A01285873@tec.mx>
2026-04-05 10:21:01 -0600

    fix: remove unused Node import in ppc.launch.py

--- manipulation/packages/manipulation_general/launch/ppc.launch.py
+++ manipulation/packages/manipulation_general/launch/ppc.launch.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription
