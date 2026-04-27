9e0e34498eee74ee979064b492f43a8cfb555188
JLDominguezM <A01285873@tec.mx>
2026-04-27 16:27:41 -0600

    fix: remove unused standing pose configuration from xarm_configurations.py fix: clean up shelf level heights in test_ppc_cabinet_place.py

--- task_manager/scripts/test/test_ppc_cabinet_place.py
+++ task_manager/scripts/test/test_ppc_cabinet_place.py
@@ -100,7 +100,6 @@ class PPCTestCabinetPlace(PickAndPlaceTM):
         self.yolo_to_logical = {v: k for k, v in self.yolo_names.items()}
 
         self.shelf_level_heights = {1: 0.475, 2: 0.827, 3: 1.201}
-
         self.default_shelf_height = 0.475
 
         try:
