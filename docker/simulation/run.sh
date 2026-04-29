274495a0cce626a1a4d69793c1497c1532d108a9
JLDominguezM <A01285873@tec.mx>
2026-04-28 19:09:46 -0600

    chore(simulation): Ensure simulation run script respects environment type

--- docker/simulation/run.sh
+++ docker/simulation/run.sh
@@ -5,7 +5,12 @@ source ../../lib.sh
 
 ARGS=("$@")  # Save all arguments in an array
 TASK=${ARGS[0]}
-ENV_TYPE="cpu" # Simulation will initially be CPU-based
+echo "TASK is: $TASK"
+ENV_TYPE="${*: -1}"
+
+if [[ ! "$ENV_TYPE" =~ ^(cpu|cuda|l4t)$ ]]; then
+  ENV_TYPE="cpu"
+fi
 
 COMPOSE="docker-compose.yaml"
 parse_common_flags "$COMPOSE" "${ARGS[@]}"
@@ -13,11 +18,22 @@ parse_common_flags "$COMPOSE" "${ARGS[@]}"
 #_________________________SETUP_________________________
 
 setup_common_env "simulation"
+add_or_update_variable .env "CYCLONE_INTERFACE" ""
+add_or_update_variable .env "BASE_IMAGE" "roborregos/home2:${ENV_TYPE}_base"
+add_or_update_variable .env "IMAGE_NAME" "roborregos/home2:simulation-${ENV_TYPE}"
+
+if [ "$ENV_TYPE" != "cpu" ]; then
+    add_or_update_variable .env "DOCKER_RUNTIME" "nvidia"
+fi
 
 #_________________________RUN_________________________
 
 SOURCE_ROS="source /opt/ros/humble/setup.bash"
-SOURCE_INSTALL="source /workspace/install/setup.bash"
+SOURCE_INSTALL="if [ -f /workspace/install/setup.bash ]; then source /workspace/install/setup.bash; fi"
+
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+# export CYCLONEDDS_URI=""
+# export CYCLONE_INTERFACE=""
 
 # Launch the MuJoCo simulation with the robot model
 # Assuming a launch file exists in mujoco_spawn or mujoco_ros2_control
@@ -26,18 +42,20 @@ RUN_SIMULATION="ros2 launch mujoco_spawn mujoco_sim_init.launch.py"
 # Default command to run if no specific task is provided
 DEFAULT_COMMAND="bash"
 
+COLCON="colcon build --symlink-install --packages-up-to mujoco_spawn mujoco_ros2_control arm_pkg perception_3d xarm_controller xarm_description frida_description xarm_msgs --packages-ignore xarm_gazebo"
+
 if [ "$BUILD" == "true" ]; then
-    COMMAND="$SOURCE_ROS && $SOURCE_INSTALL && $COLCON && $DEFAULT_COMMAND"
+    PRE_COMMAND="$COLCON && source /workspace/install/setup.bash && "
 else
-    COMMAND="$SOURCE_ROS && $SOURCE_INSTALL && $DEFAULT_COMMAND"
+    PRE_COMMAND=""
 fi
 
 case $TASK in
     "--sim")
-        COMMAND="$SOURCE_ROS && $SOURCE_INSTALL && $RUN_SIMULATION"
+        COMMAND="$SOURCE_ROS && $SOURCE_INSTALL && $PRE_COMMAND$RUN_SIMULATION"
         ;;
     *)
-        # Default command (bash) is already set up
+        COMMAND="$SOURCE_ROS && $SOURCE_INSTALL && $PRE_COMMAND$DEFAULT_COMMAND"
         ;;
 esac
 
