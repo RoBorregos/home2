2e56695e1325f7dd6b340ffc3df6bdca10932def
Champtiago <A00844931@tec.mx>
2026-06-05 16:13:12 -0600

    update display

--- robot_description/frida_description/launch/display_real.launch.py
+++ robot_description/frida_description/launch/display_real.launch.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    ld.add_action(IncludeLaunchDescription(
-        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
-        launch_arguments={
-            'urdf_package': 'frida_description',
-            'urdf_package_path': PathJoinSubstitution(['urdf','TMR2025','FRIDA_Real.urdf.xacro'])}.items()
+    # Process URDF xacro file
+    urdf_file = PathJoinSubstitution([FindPackageShare('frida_description'), 'urdf', 'TMR2025', 'FRIDA_Real.urdf.xacro'])
+    robot_description = Command(['xacro ', urdf_file])
+
+    # robot_state_publisher
+    ld.add_action(Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
     ))
+
+    # joint_state_publisher_gui
+    ld.add_action(Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    ))
+
+    # rviz2
+    ld.add_action(Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen'
+    ))
+
     return ld
