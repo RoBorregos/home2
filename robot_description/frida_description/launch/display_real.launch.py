51fd5016edbeb67953f0c48e5d555ff5944e2542
Champtiago <A00844931@tec.mx>
2026-06-05 15:48:26 -0600

    display in frame world

--- robot_description/frida_description/launch/display_real.launch.py
+++ robot_description/frida_description/launch/display_real.launch.py
@@ -4,9 +4,18 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    # Publish static transform from world to the robot base footprint
+    ld.add_action(Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '0', '0', '0', 'world', 'home_base_base_footprint'],
+        output='screen'
+    ))
 
     ld.add_action(IncludeLaunchDescription(
         PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
