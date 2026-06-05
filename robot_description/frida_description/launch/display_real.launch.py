2cff8b1b07e9d07b0a69d8cf6f3800d44395e553
Champtiago <A00844931@tec.mx>
2026-06-05 16:05:10 -0600

    ?

--- robot_description/frida_description/launch/display_real.launch.py
+++ robot_description/frida_description/launch/display_real.launch.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
 
-import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
-
-    # Publish static transform from world to the robot base footprint
-    ld.add_action(Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments=['0', '0', '0', '0', '0', '0', 'world', 'home_base_base_footprint'],
-        output='screen'
-    ))
 
     ld.add_action(IncludeLaunchDescription(
         PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
         launch_arguments={
             'urdf_package': 'frida_description',
-            'urdf_package_path': PathJoinSubstitution(['urdf','TMR2025','FRIDA_Real.urdf.xacro'])
-        }.items()
+            'urdf_package_path': PathJoinSubstitution(['urdf','TMR2025','FRIDA_Real.urdf.xacro'])}.items()
     ))
     return ld
