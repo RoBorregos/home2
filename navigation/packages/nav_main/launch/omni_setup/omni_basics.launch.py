53196e7ef4d1ad608f8f169bb956ded436a648b9
GerardoFJ <A00838645@tec.mx>
2026-06-15 01:13:44 -0600

    Update launch omno

--- navigation/packages/nav_main/launch/omni_setup/omni_basics.launch.py
+++ navigation/packages/nav_main/launch/omni_setup/omni_basics.launch.py
@@ -32,6 +32,7 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription  
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     cmd_vel_topic = LaunchConfiguration('cmd_vel_topic')
