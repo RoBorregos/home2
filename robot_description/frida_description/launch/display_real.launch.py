816e0f08269a28466892e32abd8e8622ba084659
Gerardo Fregoso Jiménez <143139229+GerardoFJ@users.noreply.github.com>
2026-06-15 00:06:57 -0600

    Revert "Omnibase-urdf (#1038) (#1039)"
    This reverts commit 3ef54cbf6de8f799b9963712cb3562d4d8e1dbb8.

--- robot_description/frida_description/launch/display_real.launch.py
+++ robot_description/frida_description/launch/display_real.launch.py
@@ -1,51 +1,17 @@
-#!/usr/bin/env python3
-from launch import LaunchDescription
-from launch.substitutions import Command, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
+ #!/usr/bin/env python3
 
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    urdf_file = PathJoinSubstitution([
-        FindPackageShare('frida_description'),
-        'urdf', 'TMR2025', 'FRIDA_Real.urdf.xacro'
-    ])
+    ld = LaunchDescription()
 
-    rviz_config = PathJoinSubstitution([
-        FindPackageShare('frida_description'),
-        'config', 'display.rviz'
-    ])
-
-    robot_description = ParameterValue(
-        Command([
-            'xacro ', urdf_file,
-            ' robot_ip:=0.0.0.0',
-            ' prefix:=',
-            ' ros2_control_plugin:=fake_components/GenericSystem',
-        ]),
-        value_type=str
-    )
-
-    return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'robot_description': robot_description,
-                'use_sim_time': False,
-            }]
-        ),
-        Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            output='screen'
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            arguments=['-d', rviz_config],
-            output='screen'
-        ),
-    ])
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+        launch_arguments={
+            'urdf_package': 'frida_description',
+            'urdf_package_path': PathJoinSubstitution(['urdf','TMR2025','FRIDA_Real.urdf.xacro'])}.items()
+    ))
+    return ld
