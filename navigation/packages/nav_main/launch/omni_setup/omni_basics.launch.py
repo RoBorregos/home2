1158ab434c84bd370933d8050b4b33ce2feb8334
GerardoFJ <A00838645@tec.mx>
2026-06-15 01:12:30 -0600

    Fix typo launch

--- navigation/packages/nav_main/launch/omni_setup/omni_basics.launch.py
+++ navigation/packages/nav_main/launch/omni_setup/omni_basics.launch.py
@@ -28,11 +28,10 @@ Prerequisites:
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription  
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 
 def generate_launch_description():
     cmd_vel_topic = LaunchConfiguration('cmd_vel_topic')
