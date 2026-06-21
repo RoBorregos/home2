ac9c6c85bd254405a25ebe329af932081580765e
Isai-JQ <a00842786@tec.mx>
2026-06-21 04:42:26 -0600

    nav test changes

--- task_manager/scripts/test/test_navigation_manager.py
+++ task_manager/scripts/test/test_navigation_manager.py
@@ -23,13 +23,13 @@ class TestNavigationManager(Node):
         )
 
         self.tests_funcs = {
-            "Check Door": {"func": self.navigation_manager.check_door},
-            "Retrieve Areas": {"func": self.navigation_manager.retrieve_areas},
-            "Move to Location": {
-                "func": self.navigation_manager.move_to_location,
-                "location": "entrance",
-                "sublocation": "",
-            },
+           # "Check Door": {"func": self.navigation_manager.check_door},
+           # "Retrieve Areas": {"func": self.navigation_manager.retrieve_areas},
+           # "Move to Location": {
+          #      "func": self.navigation_manager.move_to_location,
+           #     "location": "entrance",
+           #     "sublocation": "",
+           # },
             "Get Path Info": {
                 "func": self.navigation_manager.get_path_info,
                 "location_b": "entrance",
