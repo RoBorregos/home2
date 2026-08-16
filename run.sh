cb5a1af9dec4b2271967f3315cdffc1261074a4c
Luis Benvenuto <luiss.benvenuto@gmail.com>
2026-08-16 17:29:54 -0600

    Remove general clean (#1181)

--- run.sh
+++ run.sh
@@ -78,7 +78,7 @@ case $INPUT in
   frida_interfaces)
     run_frida_interfaces
     ;;
-  --stop|--down|--clean|--build)
+  --stop|--down|--build)
     control "$INPUT"
     ;;
   --clean-interfaces)
